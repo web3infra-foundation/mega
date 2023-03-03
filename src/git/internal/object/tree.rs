d42272899ecd16e735e0e2cea47bc390c4c91b61
Quanyi Ma <eli@patch.sh>
2023-03-03 22:59:19 +0800

    Add more comments for tree struct
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- src/git/internal/object/tree.rs
+++ src/git/internal/object/tree.rs
@@ -1,7 +1,18 @@
+//! In Git, a tree object is used to represent the state of a directory at a specific point in time.
+//! It stores information about the files and directories within that directory, including their names,
+//! permissions, and the IDs of the objects that represent their contents.
 //!
+//! A tree object can contain other tree objects as well as blob objects, which represent the contents
+//! of individual files. The object IDs of these child objects are stored within the tree object itself.
 //!
+//! When you make a commit in Git, you create a new tree object that represents the state of the
+//! repository at that point in time. The parent of the new commit is typically the tree object
+//! representing the previous state of the repository.
 //!
-//!
+//! Git uses the tree object to efficiently store and manage the contents of a repository. By
+//! representing the contents of a directory as a tree object, Git can quickly determine which files
+//! have been added, modified, or deleted between two points in time. This allows Git to perform
+//! operations like merging and rebasing more quickly and accurately.
 //!
 use std::fmt::Display;
 
@@ -11,6 +22,10 @@ use crate::git::errors::GitError;
 use crate::git::hash::Hash;
 use crate::git::internal::object::meta::Meta;
 
+/// In Git, the mode field in a tree object's entry specifies the type of the object represented by
+/// that entry. The mode is a three-digit octal number that encodes both the permissions and the
+/// type of the object. The first digit specifies the object type, and the remaining two digits
+/// specify the file mode or permissions.
 #[allow(unused)]
 #[derive(PartialEq, Eq, Hash, Ord, PartialOrd, Debug, Clone, Copy)]
 pub enum TreeItemType {
@@ -36,20 +51,51 @@ impl Display for TreeItemType {
 
 impl TreeItemType {
     #[allow(unused)]
+    /// 32-bit mode, split into (high to low bits):
+    /// - 4-bit object type: valid values in binary are 1000 (regular file), 1010 (symbolic link) and 1110 (gitlink)
+    /// - 3-bit unused
+    /// - 9-bit unix permission: Only 0755 and 0644 are valid for regular files. Symbolic links and gitlink have value 0 in this field.
     pub(crate) fn to_bytes(self) -> &'static [u8] {
         match self {
             TreeItemType::Blob => b"100644",
             TreeItemType::BlobExecutable => b"100755",
-            TreeItemType::Tree => b"40000",
             TreeItemType::Link => b"120000",
+            TreeItemType::Tree => b"040000",
             TreeItemType::Commit => b"160000",
         }
     }
 
+    /// Convert a 32-bit mode to a TreeItemType
+    ///
+    /// |0100000000000000| (040000)| Directory|
+    /// |1000000110100100| (100644)| Regular non-executable file|
+    /// |1000000110110100| (100664)| Regular non-executable group-writeable file|
+    /// |1000000111101101| (100755)| Regular executable file|
+    /// |1010000000000000| (120000)| Symbolic link|
+    /// |1110000000000000| (160000)| Gitlink|
+    /// ---
+    /// # GitLink
+    /// Gitlink, also known as a submodule, is a feature in Git that allows you to include a Git
+    /// repository as a subdirectory within another Git repository. This is useful when you want to
+    /// incorporate code from another project into your own project, without having to manually copy
+    /// the code into your repository.
+    ///
+    /// When you add a submodule to your Git repository, Git stores a reference to the other
+    /// repository at a specific commit. This means that your repository will always point to a
+    /// specific version of the other repository, even if changes are made to the submodule's code
+    /// in the future.
+    ///
+    /// To work with a submodule in Git, you use commands like git submodule add, git submodule
+    /// update, and git submodule init. These commands allow you to add a submodule to your repository,
+    /// update it to the latest version, and initialize it for use.
+    ///
+    /// Submodules can be a powerful tool for managing dependencies between different projects and
+    /// components. However, they can also add complexity to your workflow, so it's important to
+    /// understand how they work and when to use them.
     #[allow(unused)]
     pub(crate) fn tree_item_type_from(mode: &[u8]) -> Result<TreeItemType, GitError> {
         Ok(match mode {
-            b"40000" => TreeItemType::Tree,
+            b"040000" => TreeItemType::Tree,
             b"100644" => TreeItemType::Blob,
             b"100755" => TreeItemType::BlobExecutable,
             b"120000" => TreeItemType::Link,
@@ -65,6 +111,24 @@ impl TreeItemType {
     }
 }
 
+/// A tree object contains a list of entries, one for each file or directory in the tree. Each entry
+/// in the file represents an entry in the tree, and each entry has the following format:
+///
+/// ```bash
+/// <mode> <name>\0<binary object ID>
+/// ```
+/// - `<mode>` is the mode of the object, represented as a six-digit octal number. The first digit
+/// represents the object type (tree, blob, etc.), and the remaining digits represent the file mode or permissions.
+/// - `<name>` is the name of the object.
+/// - `\0` is a null byte separator.
+/// - `<binary object ID>` is the ID of the object that represents the contents of the file or
+/// directory, represented as a binary SHA-1 hash.
+///
+/// # Example
+/// ```bash
+/// 100644 hello-world\0<blob object ID>
+/// 040000 data\0<tree object ID>
+/// ```
 #[allow(unused)]
 #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
 pub struct TreeItem {
@@ -74,7 +138,10 @@ pub struct TreeItem {
     pub filename: String,
 }
 
+/// A tree object is a Git object that represents a directory. It contains a list of entries, one
+/// for each file or directory in the tree.
 #[allow(unused)]
+#[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
 pub struct Tree {
     pub meta: Meta,
     pub tree_items: Vec<TreeItem>,
