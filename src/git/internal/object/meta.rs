4f16446128e2f41de31e8c0125ceb2b5d8f7b374
Quanyi Ma <eli@patch.sh>
2023-03-03 11:33:48 +0800

    Fix bug: Hashing without object type and length
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- src/git/internal/object/meta.rs
+++ src/git/internal/object/meta.rs
@@ -37,45 +37,49 @@ pub struct Meta {
     pub id: Hash,
     pub size: usize,
     pub data: Vec<u8>,
-    pub delta_header: Vec<u8>,
 }
 
 impl Meta {
-    /// Create a new `Meta` struct.
+    /// Create a new `Meta` struct from a Git object include object type and data.
     /// # Examples
     /// ```
-    ///     let meta = Meta::new(ObjectType::Blob, 0, vec![], vec![]);
+    ///     let meta = Meta::new(ObjectType::Blob, vec![98, 108, 111, 98, 32, 49, 52, 0, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33, 10]);
     ///     assert_eq!(meta.object_type, ObjectType::Blob);
-    ///     assert_eq!(meta.id, Hash::new());
-    ///     assert_eq!(meta.size, 0);
-    ///     assert_eq!(meta.data, vec![]);
+    ///     assert_eq!(meta.id.to_plain_str(), "8ab686eafeb1f44702738c8b0f24f2567c36da6d");
+    ///     assert_eq!(meta.size, 14);
     ///     assert_eq!(meta.delta_header, vec![]);
     /// ```
     #[allow(unused)]
-    pub fn new(
-        object_type: ObjectType,
-        size: usize,
-        data: Vec<u8>,
-        delta_header: Vec<u8>,
-    ) -> Self {
+    fn new_from_data(object_type: ObjectType, data: Vec<u8>) -> Self {
         Meta {
             object_type,
-            id: Hash::new(&data),
-            size,
+            id: Meta::calculate_id(object_type, data.clone()),
+            size: data.len(),
             data,
-            delta_header,
         }
     }
 
-    #[allow(unused)]
-    pub fn new_from_data(object_type: ObjectType, data: Vec<u8>) -> Self {
-        Meta {
-            object_type,
-            id: Hash::new(&data),
-            size: data.len(),
-            data,
-            delta_header: vec![],
-        }
+    /// A Git object in the Loose Format consists of two parts: the object header and the object data.
+    /// The object header is stored in plain text format and contains the following information:
+    ///
+    /// - Object type: type of Git object the object is, such as a blob, tree, commit, or tag.
+    /// - Object size: size of the object data in bytes.
+    ///
+    /// The object header has a space(`\x32`) between the object type and the object size.
+    ///
+    /// The object header is followed by a null byte (0x00) and then the object data.
+    ///
+    /// The object id is calculated from the object header and the object data.
+    fn calculate_id(object_type: ObjectType, data: Vec<u8>) -> Hash {
+        let mut d: Vec<u8> = Vec::new();
+
+        d.extend(object_type.to_bytes().unwrap());
+        d.push(b' ');
+        d.extend(data.len().to_string().as_bytes());
+        d.push(b'\x00');
+        d.extend(data);
+
+        Hash::new(&d)
     }
 
     #[allow(unused)]
@@ -97,18 +101,8 @@ impl Meta {
     /// The Loose Format is suitable for storing a small number of Git objects, or in cases where Git
     /// objects do not need to be frequently read or modified.
     ///
-    /// A Git object in the Loose Format consists of two parts: the object header and the object data.
-    /// The object header is stored in plain text format in the first few lines of the object file,
-    /// and contains the following information:
-    ///
-    /// - Object type: type of Git object the object is, such as a Blob, Tree, Commit, or Tag.
-    /// - Object size: size of the object data in bytes.
-    /// - Object data: the actual content.
-    ///
-    /// |Object Type|Object Size|Object Data|
-    /// |-----------|-----------|-----------|
-    /// |blob|13 bytes|"Hello, World!"|
-    ///
+    /// TODO: Add a overwrite flag to control whether to overwrite the existing file.
+    /// TODO: Add a file path parameter to control where to store the file without flow Git store spec.
     #[allow(unused)]
     pub fn  loose_2file(&self, root: &str) -> Result<String, GitError> {
         // e is a ZlibEncoder, which is a wrapper around a Writer that compresses the data written to
@@ -147,6 +141,14 @@ impl Meta {
         Ok(path.to_str().unwrap().to_string())
     }
 
+    /// # Attention
+    /// In the ASCII character set, the character corresponding 10(hex: 0x0A) is the line feed (LF)
+    /// character, which is commonly used as a symbol for a new line in text files. The LF character
+    /// is represented by the hexadecimal value of 0x0A in ASCII. The way that new lines are
+    /// represented in text files can vary across different operating systems and programming
+    /// languages. In Linux and Unix systems, LF is commonly used to represent new lines in text
+    /// files, while in Windows systems, a combination of carriage return (CR) and LF ("\r\n") is
+    /// commonly used.
     #[allow(unused)]
     pub fn new_from_file(path: &str) -> Result<Self, GitError> {
         let file = File::open(path).unwrap();
@@ -180,87 +182,9 @@ impl Meta {
             )),
         }
     }
-
-    #[allow(unused)]
-    pub fn new_from_data_and_delta_header(
-        object_type: ObjectType,
-        data: Vec<u8>,
-        delta_header: Vec<u8>,
-    ) -> Self {
-        Meta {
-            object_type,
-            id: Hash::new(&data),
-            size: data.len(),
-            data,
-            delta_header,
-        }
-    }
-
-    #[allow(unused)]
-    pub fn pack_2vec(&self) -> Result<Vec<u8>, GitError> {
-        let mut data =
-            // `<< 4` This left-shifts the result of type2number() by 4 bits, effectively multiplying
-            // it by 16. This leaves the 4 least significant bits of the byte set to 0.
-            // `0x80 | ...` This sets the most significant bit of the byte to 1, indicating that the
-            // byte represents a Git object. Specifically, the value 0x80 has its most significant
-            // bit set to 1, and all other bits set to 0.
-            vec![(0x80 | (self.object_type.type2number() << 4))
-                // + (self.size & 0x0f) as u8: This sets the 4 least significant bits of the byte
-                // to the low-order 4 bits of the object's size. Specifically, self.size & 0x0f
-                // performs a bitwise AND operation between self.size and the hexadecimal value 0x0f,
-                // which results in the low-order 4 bits of self.size. This value is then cast to a
-                // u8 byte and added to the byte constructed in steps 2 and 3.
-                + (self.size & 0x0f) as u8];
-
-        let mut s = self.size >> 4;
-
-        if s > 0 {
-            while s > 0 {
-                if s >> 7 > 0 {
-                    data.push((0x80 | s) as u8);
-                    s >>= 7;
-                } else {
-                    data.push((s) as u8);
-                    break;
-                }
-            }
-        } else {
-            data.push(0);
-        }
-
-        match self.object_type {
-            ObjectType::OffsetDelta => {
-                data.append(&mut self.delta_header.clone());
-            }
-            ObjectType::HashDelta => {
-                data.append(&mut self.delta_header.clone());
-            }
-            _ => {}
-        }
-
-        let mut encoder = ZlibEncoder::new(Vec::new(), Compression::Default);
-        encoder.write_all(&self.data).expect("Write error!");
-        data.append(&mut encoder.finish().expect("Failed to finish compression!"));
-
-        Ok(data)
-    }
 }
 
 mod tests {
-    #[test]
-    fn test_meta_new() {
-        use crate::git::internal::ObjectType;
-
-        let meta = super::Meta::new(ObjectType::Blob,0,
-                                    vec![], vec![]);
-
-        assert_eq!(meta.object_type, ObjectType::Blob);
-        assert_eq!(meta.id.to_plain_str(), "da39a3ee5e6b4b0d3255bfef95601890afd80709");
-        assert_eq!(meta.size, 0);
-        assert_eq!(meta.data.len(), 0);
-        assert_eq!(meta.delta_header.len(), 0);
-    }
-
     #[test]
     fn test_new_from_data() {
         use crate::git::internal::ObjectType;
@@ -270,22 +194,7 @@ mod tests {
 
         assert_eq!(meta.object_type, ObjectType::Blob);
         assert_eq!(meta.size, 13);
-        assert_eq!(meta.id.to_plain_str(), "0a0a9f2a6772942557ab5355d76af442f8f65e01");
-    }
-
-    #[test]
-    fn test_new_from_data_with_delta() {
-        use crate::git::internal::ObjectType;
-
-        let meta = super::Meta::new_from_data_and_delta_header(
-            ObjectType::OffsetDelta,
-            "Hello, World!".as_bytes().to_vec(),
-            vec![0x00, 0x00, 0x00, 0x00]);
-
-        assert_eq!(meta.object_type, ObjectType::OffsetDelta);
-        assert_eq!(meta.size, 13);
-        assert_eq!(meta.id.to_plain_str(), "0a0a9f2a6772942557ab5355d76af442f8f65e01");
-        assert_eq!(meta.delta_header, vec![0x00, 0x00, 0x00, 0x00]);
+        assert_eq!(meta.id.to_plain_str(), "b45ef6fec89518d314f546fd6c3025367b721684");
     }
 
     #[test]
@@ -295,7 +204,7 @@ mod tests {
         let meta = super::Meta::new_from_data(ObjectType::Blob,
                                               "Hello, World!".as_bytes().to_vec());
 
-        assert_eq!(meta.to_folder_name(), "0a");
+        assert_eq!(meta.to_folder_name(), "b4");
     }
 
     #[test]
@@ -305,62 +214,62 @@ mod tests {
         let meta = super::Meta::new_from_data(ObjectType::Blob,
                                               "Hello, World!".as_bytes().to_vec());
 
-        assert_eq!(meta.to_file_name(), "0a9f2a6772942557ab5355d76af442f8f65e01");
-    }
-    
-    #[test]
-    fn test_loose_2file() {
-        //8ab686eafeb1f44702738c8b0f24f2567c36da6d
-
-        use crate::git::internal::ObjectType;
-        use std::env;
-        use std::path::PathBuf;
-        use std::fs::remove_file;
-
-        let mut project = PathBuf::from(env::current_dir().unwrap());
-        project.push("tests/objects");
-
-        let mut dest = PathBuf::from(env::current_dir().unwrap());
-        dest.push("tests/objects/0a/0a9f2a6772942557ab5355d76af442f8f65e01");
-        if dest.exists() {
-            remove_file(dest.as_path().to_str().unwrap()).unwrap();
-        }
-
-        let meta = super::Meta::new_from_data(ObjectType::Blob,
-                                              "Hello, World!".as_bytes().to_vec());
-
-        let file = meta.loose_2file(project.as_path().to_str().unwrap()).unwrap();
-
-        assert_eq!(file, dest.as_path().to_str().unwrap());
+        assert_eq!(meta.to_file_name(), "5ef6fec89518d314f546fd6c3025367b721684");
     }
 
     #[test]
+    #[cfg(any(unix))]
     fn test_new_from_file() {
+        use std::env;
+        use std::path::PathBuf;
         use crate::git::internal::ObjectType;
+
+        // "Hello, World!" is [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33] without
+        // line feed(LF), which is a control character used to represent the end of a line of text and the
+        // beginning of a new line. The LF character is commonly used in Unix and Unix-like operating
+        // systems (including Linux and macOS) as the standard end-of-line marker in text files
+        //
+        // So, When read a file include the same content "Hello, World!", it's different SHA-1
+        // value calculated.
+        //
+        // The object is stored in the tests/data/objects/8a/b686eafeb1f44702738c8b0f24f2567c36da6d
+        // In Linux and Unix systems, LF is commonly used to represent new lines in text files, while
+        // in Windows systems, a combination of carriage return (CR) and LF ("\r\n") is commonly used.
+        //
+        // "Hello, World!" is [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+        // "Hello, World!" read from file is [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33, 10]
+        let mut project = PathBuf::from(env::current_dir().unwrap());
+        project.push("tests/data/objects/8a/b686eafeb1f44702738c8b0f24f2567c36da6d");
+
+        let meta = super::Meta::new_from_file(project.as_path().to_str().unwrap()).unwrap();
+
+        assert_eq!(meta.object_type, ObjectType::Blob);
+        assert_eq!(meta.size, 14);
+        assert_eq!(meta.id.to_plain_str(), "8ab686eafeb1f44702738c8b0f24f2567c36da6d");
+    }
+
+    #[test]
+    #[cfg(any(unix))]
+    fn test_loose_2file() {
         use std::env;
         use std::path::PathBuf;
         use std::fs::remove_file;
 
-        //1c03657b57ce1ba77b9243e48687e9f7183acd86
+        let mut source = PathBuf::from(env::current_dir().unwrap());
+        source.push("tests/data/objects/8a/b686eafeb1f44702738c8b0f24f2567c36da6d");
+        let m = super::Meta::new_from_file(source.as_path().to_str().unwrap()).unwrap();
 
-        let mut project = PathBuf::from(env::current_dir().unwrap());
-        project.push("tests/objects");
-
-        let mut dest = PathBuf::from(env::current_dir().unwrap());
-        dest.push("tests/objects/89/b06a0467f41bb25165467f01a8fc86d94436e8");
-        if dest.exists() {
-            remove_file(dest.as_path().to_str().unwrap()).unwrap();
+        let mut dest_file = PathBuf::from(env::current_dir().unwrap());
+        dest_file.push("tests/objects/8a/b686eafeb1f44702738c8b0f24f2567c36da6d");
+        if dest_file.exists() {
+            remove_file(dest_file.as_path().to_str().unwrap()).unwrap();
         }
 
-        let meta = super::Meta::new_from_data(ObjectType::Blob,
-                                              "Hello, Rust!".as_bytes().to_vec());
+        let mut dest = PathBuf::from(env::current_dir().unwrap());
+        dest.push("tests/objects");
+        let file = m.loose_2file(dest.as_path().to_str().unwrap()).unwrap();
 
-        let file = meta.loose_2file(project.as_path().to_str().unwrap()).unwrap();
-
-        let meta2 = super::Meta::new_from_file(file.as_str()).unwrap();
-
-        assert_eq!(meta2.object_type, ObjectType::Blob);
-        assert_eq!(meta2.size, 12);
-        assert_eq!(meta2.id.to_plain_str(), "89b06a0467f41bb25165467f01a8fc86d94436e8");
+        dest.push("8a/b686eafeb1f44702738c8b0f24f2567c36da6d");
+        assert_eq!(file, dest.as_path().to_str().unwrap());
     }
 }
