9be7d8a3219746bf95e5373c31ab005c2da2891c
Quanyi Ma <eli@patch.sh>
2023-03-08 23:02:37 +0800

    Use signature struct for author and committer
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- src/git/internal/object/commit.rs
+++ src/git/internal/object/commit.rs
@@ -3,19 +3,17 @@
 //!
 //!
 //!
-
-
 use crate::git::hash::Hash;
 use crate::git::internal::object::meta::Meta;
-use crate::git::internal::object::signature::AuthorSignature;
+use crate::git::internal::object::signature::Signature;
 
 #[allow(unused)]
 pub struct Commit {
     pub meta: Meta,
     pub tree_id: Hash,
     pub parent_tree_ids: Vec<Hash>,
-    pub author: AuthorSignature,
-    pub committer: AuthorSignature,
+    pub author: Signature,
+    pub committer: Signature,
     pub message: String,
 }
 
