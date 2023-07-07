2099fbc973f4346989890c47c9cbdeec49cafad0
Xiaoyang Han <lux1an@qq.com>
2023-07-07 14:49:37 +0800

    Change decode operation.
    Signed-off-by: Xiaoyang Han <lux1an@qq.com>

--- git/src/protocol/mod.rs
+++ git/src/protocol/mod.rs
@@ -8,7 +8,7 @@ pub mod pack;
 pub mod ssh;
 
 use std::{
-    io::{BufReader, Cursor},
+    io::Cursor,
     path::PathBuf,
     str::FromStr,
     sync::Arc,
@@ -16,17 +16,17 @@ use std::{
 
 use database::driver::{mysql::storage::MysqlStorage, ObjectStorage};
 
-use crate::protocol::pack::SP;
+use crate::{
+    errors::GitError,
+    internal::{object::GitObjects, pack::decode::HashCounter},
+    protocol::pack::SP,
+};
 
 use bytes::Bytes;
 use entity::refs;
 use sea_orm::{ActiveValue::NotSet, Set};
 
-use crate::internal::{
-    object::{blob::Blob, commit::Commit, tag::Tag, tree::Tree, ObjectT},
-    pack::{decode::ObjDecodedMap, iterator::EntriesIter, Pack},
-    ObjectType,
-};
+use crate::internal::pack::{decode::ObjDecodedMap, iterator::EntriesIter, Pack};
 use common::{errors::MegaError, utils::ZERO_ID};
 
 #[derive(Clone)]
@@ -181,28 +181,46 @@ impl RefCommand {
         pack_file: &mut Bytes,
         // storage: &T,
     ) -> Result<ObjDecodedMap, anyhow::Error> {
-        match Pack::decode(&mut Cursor::new(&pack_file)).await {
-            Ok(decoded_pack) => {
-                let inner = &mut Cursor::new(&pack_file);
-                let mut inter = EntriesIter::new(
-                    BufReader::with_capacity(4096, inner),
-                    decoded_pack.number_of_objects() as u32,
-                );
-                let mut object_map = ObjDecodedMap::default();
-                for _ in 0..decoded_pack.number_of_objects() {
-                    let obj = inter.next_obj().await?;
-                    println!("{}", obj);
-                    let raw = obj.get_raw().to_vec();
+        let obj: Result<ObjDecodedMap, GitError> = {
+            let mut object_map = ObjDecodedMap::default();
+            let count_hash: bool = true;
+            let curosr_pack = Cursor::new(pack_file);
+            let mut reader = HashCounter::new(curosr_pack, count_hash);
+            // Read the header of the pack file
+            let mut pack = Pack::check_header(&mut reader)?;
 
-                    match obj.get_type() {
-                        ObjectType::Commit => object_map.commits.push(Commit::new_from_data(raw)),
-                        ObjectType::Tree => object_map.trees.push(Tree::new_from_data(raw)),
-                        ObjectType::Blob => object_map.blobs.push(Blob::new_from_data(raw)),
-                        ObjectType::Tag => object_map.tags.push(Tag::new_from_data(raw)),
-                        _ => todo!(),
+            let mut iterator = EntriesIter::new(&mut reader, pack.number_of_objects() as u32);
+            for _ in 0..pack.number_of_objects() {
+                let obj = iterator.next_git_obj().await?;
+                println!("{}", obj);
+                match obj {
+                    GitObjects::COMMIT(a) => {
+                        object_map.commits.push(a);
+                    }
+                    GitObjects::TREE(a) => {
+                        object_map.trees.push(a);
+                    }
+                    GitObjects::BLOB(a) => {
+                        object_map.blobs.push(a);
+                    }
+                    GitObjects::TAG(a) => {
+                        object_map.tags.push(a);
                     }
                 }
+            }
+            drop(iterator);
 
+            let _hash = reader.final_hash();
+            pack.signature = _hash;
+            //pack.signature = Hash::new_from_bytes(&id[..]);
+
+            // pack.signature = read_tail_hash(&mut reader);
+            // assert_eq!(_hash, pack.signature);
+
+            Ok(object_map)
+        };
+        match obj {
+            Ok(object_map) => {
                 self.status = RefCommand::OK_STATUS.to_owned();
                 Ok(object_map)
             }
@@ -212,6 +230,37 @@ impl RefCommand {
                 Err(err.into())
             }
         }
+
+        // match Pack::decode(&mut Cursor::new(&pack_file)).await {
+        //     Ok(decoded_pack) => {
+        //         let inner = &mut Cursor::new(&pack_file);
+        //         let mut inter = EntriesIter::new(
+        //             BufReader::with_capacity(4096, inner),
+        //             decoded_pack.number_of_objects() as u32,
+        //         );
+
+        //         for _ in 0..decoded_pack.number_of_objects() {
+        //             let obj = inter.next_obj().await?;
+        //             println!("{}", obj);
+        //             let raw = obj.get_raw().to_vec();
+        //             //let rrr: Arc<Mutex<dyn Any>> = Arc::new(Mutex::new(_blob));
+        //             match obj.get_type() {
+        //                 ObjectType::Commit => object_map.commits.push(Commit::new_from_data(raw)),
+        //                 ObjectType::Tree => object_map.trees.push(Tree::new_from_data(raw)),
+        //                 ObjectType::Blob => object_map.blobs.push(Blob::new_from_data(raw)),
+        //                 ObjectType::Tag => object_map.tags.push(Tag::new_from_data(raw)),
+        //                 _ => todo!(),
+        //             }
+        //         }
+
+        //         self.status = RefCommand::OK_STATUS.to_owned();
+        //         Ok(object_map)
+        //     }
+        //     Err(err) => {
+        //         self.status = RefCommand::FAILED_STATUS.to_owned();
+        //         self.error_msg = err.to_string();
+        //         Err(err.into())
+        //     }
     }
 
     pub fn get_status(&self) -> String {
@@ -245,7 +294,6 @@ impl RefCommand {
         }
     }
 }
-
 impl PackProtocol {
     pub fn new(path: PathBuf, storage: Arc<dyn ObjectStorage>, protocol: Protocol) -> Self {
         PackProtocol {
