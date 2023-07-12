5818885dbfe85c8ce386f27a16135a038c676c05
YutingNie <104416402+YutingNie@users.noreply.github.com>
2023-07-12 21:17:33 +0800

    Add code-view, code-review react component (#55)
    Signed-off-by: Yuting Nie <yvettemisaki@outlook.com>

---
+++ ui/src/index.js
@@ -0,0 +1,14 @@
+import React, { StrictMode, useState, useEffect } from "react";
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+);
+
+reportWebVitals();
