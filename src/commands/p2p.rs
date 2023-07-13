bee78050d450f9cfad2b3b9abb4ba7d42ad26df7
吴剑 <353981613@qq.com>
2023-07-13 18:29:21 +0800

    add p2p function
    Signed-off-by: 吴剑 <353981613@qq.com>
    Signed-off-by: wujian <353981613@qq.com>
    Signed-off-by: wujian0327 <353981613@qq.com>

---
+++ src/commands/p2p.rs
@@ -0,0 +1,28 @@
+//!
+//!
+//!
+//!
+//!
+use clap::{ArgMatches, Args, Command, FromArgMatches};
+
+use crate::{cli::Config};
+use common::errors::MegaResult;
+
+use p2p::peer;
+
+pub fn cli() -> Command {
+    peer::P2pOptions::augment_args_for_update(Command::new("p2p").about("Start p2p node"))
+}
+
+#[tokio::main]
+pub(crate) async fn exec(_config: Config, args: &ArgMatches) -> MegaResult {
+    let server_matchers = peer::P2pOptions::from_arg_matches(args)
+        .map_err(|err| err.exit())
+        .unwrap();
+    println!("{server_matchers:#?}");
+    peer::run(&server_matchers).await.unwrap();
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {}
