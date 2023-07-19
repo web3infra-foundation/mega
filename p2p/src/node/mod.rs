7be43b9d7906239cd2c2669ef4d8072a6d0270f3
wujian0327 <353981613@qq.com>
2023-07-19 11:32:14 +0800

    add p2p function
    Signed-off-by: wujian0327 <353981613@qq.com>

---
+++ p2p/src/node/mod.rs
@@ -0,0 +1,15 @@
+//!
+//!
+//!
+//!
+//!
+//!
+
+pub mod client;
+mod lib;
+pub mod relay_server;
+
+#[cfg(test)]
+mod tests {}
+
+
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -4,6 +4,7 @@
 //!
 //!
 mod https;
+mod p2p;
 mod ssh;
 
 use clap::{ArgMatches, Command};
@@ -12,13 +13,14 @@ use crate::cli::Config;
 use common::errors::MegaResult;
 
 pub fn builtin() -> Vec<Command> {
-    vec![https::cli(), ssh::cli()]
+    vec![https::cli(), ssh::cli(), p2p::cli()]
 }
 
 pub(crate) fn builtin_exec(cmd: &str) -> Option<fn(Config, &ArgMatches) -> MegaResult> {
     let f = match cmd {
         "https" => https::exec,
         "ssh" => ssh::exec,
+        "p2p" => p2p::exec,
         _ => return None,
     };
 
