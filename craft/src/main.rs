57d8dfae9de4f9dd9a01f5cc7a5a8fb92d8d78ae
Jiajun Li <frankanepc@gmail.com>
2023-08-09 12:45:27 +0800

    feat: craft add generate-key with primary id
    Signed-off-by: Jiajun Li <frankanepc@gmail.com>

--- craft/src/main.rs
+++ craft/src/main.rs
@@ -35,12 +35,12 @@ fn main() -> Result<(), anyhow::Error> {
         // Encrypt file contents with a public key
         "encrypt" => {
             // Encrypt blob.data
-            let _ = encrypt_blob(&args.file[1]);
+            let _ = encrypt_blob(&args.file[1],&args.file[2]);
         }
         // Decrypt file contents with a secret key
         "decrypt" => {
             //Decrypt blob.data
-            let _ =decrypt_blob(&args.file[1]);
+            let _ =decrypt_blob(&args.file[1],&args.file[2]);
         }
         "list-keys" => {
             //Show key lists
@@ -48,7 +48,7 @@ fn main() -> Result<(), anyhow::Error> {
         }
         "delete-key" => {
             //Delete key by fingerprint
-            let _ =delete_key(&args.file[1]);
+            let _ =delete_key(&args.file[1],&args.file[2]);
         }
         // For any other mode, print an error message and exit
         _ => {
@@ -83,7 +83,7 @@ mod tests {
     fn test_encrypt() {
         generate_key();
         // Create a mock argument vector with encrypt as the first element
-        let _ = encrypt_blob("../tests/data/objects/message.txt");
+        let _ = encrypt_blob("../tests/data/objects/message.txt","../craft/key_files/pub.asc");
         // Read the contents of the message.txt file and assert it is not empty
         let message = std::fs::read_to_string("../tests/data/objects/message.txt").unwrap();
         std::fs::write("../tests/objects/encrypt.txt", message).expect("Unable to write test encrypt output");
@@ -92,15 +92,15 @@ mod tests {
         // Check if the contents are encrypted by looking for the PGP header
         assert!(message.starts_with("-----BEGIN PGP MESSAGE-----"));
         //Decrypt it to do next test
-        let _ = decrypt_blob("../tests/data/objects/message.txt");
+        let _ = decrypt_blob("../tests/data/objects/message.txt","../craft/key_files/sec.asc");
     }
 
     // Define a test function for decrypt mode
     # [test]
     fn test_decrypt() {
         generate_key();
-        let _ = encrypt_blob("../tests/data/objects/message.txt");
-        let _ = decrypt_blob("../tests/data/objects/message.txt");
+        let _ = encrypt_blob("../tests/data/objects/message.txt","../craft/key_files/pub.asc");
+        let _ = decrypt_blob("../tests/data/objects/message.txt","../craft/key_files/sec.asc");
         // Read the contents of the message.txt file and assert it is not empty
         let message = std::fs::read_to_string("../tests/data/objects/message.txt").unwrap();
         std::fs::write("../tests/objects/decrypt.txt", message).expect("Unable to write test encrypt output");
@@ -108,8 +108,6 @@ mod tests {
         assert!(!message.is_empty());
         // Check if the contents are decrypted by looking for the plain text
         assert!(message.starts_with("This is a test message."));
-        // Encrypt it to do next test
-       // let _ = encrypt_blob("../tests/data/objects/emessage.txt");
     }
 
     // Define a test function for list-keys mode
@@ -124,9 +122,10 @@ mod tests {
     // Define a test function for delete-key mode
     # [test]
     fn test_delete_key() {
+        generate_key();
         // Create a mock argument vector with delete-key as the first element and a valid fingerprint as the second element
-        let data = delete_key("F6B9C0F1E8A7D3B8C6E2E0F9A5A4D8C7B7C6D5A4").unwrap();
+        let data = delete_key("../craft/key_files/pub.asc","../craft/key_files/sec.asc").unwrap();
         // Capture the standard output and assert it is not empty
-        assert_eq!(data, "Key F6B9C0F1E8A7D3B8C6E2E0F9A5A4D8C7B7C6D5A4 deleted successfully");
+        assert!(!data.is_empty());
     }
 }
