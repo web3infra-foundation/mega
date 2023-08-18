6aad04ec98be3fab9bece08de7a17fceb72d769c
Xiaoyang Han <lux1an@qq.com>
2023-08-18 21:34:32 +0800

    Decoding process configuration information acquisition
    Signed-off-by: Xiaoyang Han <lux1an@qq.com>

--- git/src/internal/pack/cqueue.rs
+++ git/src/internal/pack/cqueue.rs
@@ -1,4 +1,5 @@
 /// A circular queue data structure that holds elements of generic type `T`.
+#[derive(Debug)]
 pub struct CircularQueue<T> {
     data: Vec<Option<T>>, // Storage for the queue elements
     cap: usize, // Capacity of the queue
@@ -32,6 +33,16 @@ impl<T> CircularQueue<T> {
             Ok(())
         }
     }
+    #[allow(unused)]
+    pub fn enequeue_force(&mut self, value: T)  {
+        if self.is_full(){
+            self.read_index = (self.read_index + 1) % (self.cap+1);
+        }
+        {self.data[self.write_index].take();}
+        self.data[self.write_index]=Some(value);
+        self.write_index = (self.write_index + 1) % (self.cap+1);
+        
+    }
 
     /// Dequeues an element from the circular queue.
     /// Returns `Some(T)` if the queue is not empty, or `None` if the queue is empty.
@@ -50,7 +61,10 @@ impl<T> CircularQueue<T> {
     pub fn front(&self) -> Option<&T> {
         self.data[self.read_index].as_ref()
     }
-
+    #[allow(unused)]
+    pub fn tail(&self) -> Option<&T> {
+        self.data[(self.write_index-1)%(self.cap+1)].as_ref()
+    }
     /// Checks if the circular queue is empty.
     pub fn is_empty(&self) -> bool {
         self.read_index == self.write_index
@@ -85,6 +99,7 @@ mod tests {
 
         for i in 1..4 {
             circular_queue.enqueue(i).unwrap();
+
         }
         //tail 3 2 1 front 
         assert!(circular_queue.is_full());
@@ -96,5 +111,26 @@ mod tests {
         let result = circular_queue.front();
         assert_eq!(result, Some(&2));
 
+    }
+
+    #[test]
+    fn test_force_en_queue(){
+        let mut circular_queue = CircularQueue::new(10);
+        for i in 1..=20 {
+            circular_queue.enequeue_force(i);
+            //println!("{:?}",circular_queue)
+        }
+        let front= circular_queue.front();
+        assert_eq!(front,Some(&11));
+        let tail = circular_queue.tail();
+        assert_eq!(tail,Some(&20));
+
+        for i in 21..=35{
+            circular_queue.enequeue_force(i);
+        }
+        let front= circular_queue.front();
+        assert_eq!(front,Some(&26));
+        let tail = circular_queue.tail();
+        assert_eq!(tail,Some(&35));
     }
 }
