0d7b0ad14dc7610d5a53e4c69d56b573f6db55c5
Yuting Nie <yvettemisaki@outlook.com>
2023-08-18 21:31:41 +0800

    Updata front-end capabilities
    Signed-off-by: Yuting Nie <yvettemisaki@outlook.com>

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,17 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html', 
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-  
+  plugins: [],
 }
