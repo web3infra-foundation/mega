757f5a0c3dbe56421103061510d8b104300f1a89
sonichen <1606673007@qq.com>
2023-09-03 16:11:44 +0800

    mda: fix generation bugs
    Signed-off-by: sonichen <1606673007@qq.com>

--- mda/src/run_mda.rs
+++ mda/src/run_mda.rs
@@ -10,7 +10,7 @@ use crate::generate::{
     generate_mda_separate_annotations_one_to_many,
     generate_mda_separate_annotations_one_to_many_in_folder,
 };
- use crate::read_from_folders::*;
+use crate::read_from_folders::*;
 use crate::update::update_anno_in_combined_file;
 use crate::*;
 use crate::{extract::read_info_from_mda, update::update_anno_in_mda};
@@ -19,38 +19,38 @@ use indicatif::ProgressBar;
 use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::error::Error;
- 
+
 use std::path::Path;
 /// Command Line Tool
 #[derive(Parser, Debug)]
-#[command(version = "0.1.0", about = "", long_about = "", after_help = "")]
+#[command(version = "0.1.0", about = "", long_about = "Design and Implementation of File for AI Training Data", after_help = "")]
 #[derive(Deserialize, Serialize)]
 pub struct MDAOptions {
-    /// 4 actions: generate, extract, list,
+    /// 5 actions: generate, extract, list, group, version, update
     #[arg(long)]
     pub action: String,
 
-    /// The path to train data
+    /// Training data file/folder path.
     #[arg(long)]
     pub train: Option<String>,
 
-    /// The path to annotation data
-
+    /// Annotation data file/folder path.
     #[arg(long)]
     pub anno: Option<String>,
 
+    /// Annotation data file/folder path, separated by commas
     #[arg(long)]
     pub annos: Option<String>,
 
-    /// The path output file
+    /// Output data file/folder path.
     #[arg(long)]
     pub output: Option<String>,
 
-    /// The path to .mda file
+    /// MDA data file/folder path.
     #[arg(long)]
     pub mda: Option<String>,
 
-    /// The special version
+    /// Tags for MDA files
     #[arg(long)]
     pub tags: Option<String>,
 
@@ -58,13 +58,9 @@ pub struct MDAOptions {
     #[arg(long, default_value = "10")]
     pub threads: Option<usize>,
 
-    /// Maximum number of threads  
+    /// The version of MDA file
     #[arg(long, default_value = "-1")]
     pub rev: Option<i32>,
-
-    /// The special version
-    #[arg(long)]
-    pub add_tags: Option<String>,
 
     /// Read from which line of the annotation file.
     #[arg(long, default_value = "1")]
@@ -74,17 +70,22 @@ pub struct MDAOptions {
     #[arg(long, default_value = "0")]
     pub end: Option<usize>,
 
-    /// The type of the annotation data, txt,json
+    /// The type of the annotation data: txt,json
     #[arg(long, default_value = "txt")]
     pub format: Option<String>,
 
-    /// The type of the annotation data, txt,json
+    /// Combined Annotation data config.
     #[arg(long)]
     pub anno_config: Option<String>,
 
-    /// The type of the annotation data, txt,json
+    /// The group of the annotation data
     #[arg(long, default_value = "NONE")]
     pub group: Option<String>,
+
+    /// The generation mode: one, multiple, combine
+    #[arg(long)]
+    pub mode: Option<String>,
+ 
 }
 #[allow(unused_assignments)]
 pub fn run(config: MDAOptions) -> Result<(), Box<dyn Error>> {
@@ -101,7 +102,7 @@ pub fn run(config: MDAOptions) -> Result<(), Box<dyn Error>> {
                     for file in mda_files {
                         match read_info_from_mda(&file) {
                             Ok((index, header)) => {
-                                table = print_table_cell(table.clone(), index, header);
+                                table = print_table_cell(&file,table.clone(), index, header);
                             }
                             Err(err) => {
                                 eprintln!(
@@ -118,7 +119,7 @@ pub fn run(config: MDAOptions) -> Result<(), Box<dyn Error>> {
                     match read_info_from_mda(mda) {
                         Ok((index, header)) => {
                             let table = print_table_header();
-                            let table = print_table_cell(table, index, header);
+                            let table = print_table_cell(mda,table, index, header);
                             table.printstd();
                         }
                         Err(err) => {
@@ -130,8 +131,6 @@ pub fn run(config: MDAOptions) -> Result<(), Box<dyn Error>> {
                             );
                         }
                     }
-                } else {
-                    // do nothing
                 }
             }
             _ => {
@@ -205,21 +204,22 @@ pub fn run(config: MDAOptions) -> Result<(), Box<dyn Error>> {
                     }
                     1
                 } else if is_file(anno_data) && is_directory(mda) {
-                   match update_anno_in_combined_file(
+
+                    
+                    match update_anno_in_combined_file(
                         mda,
                         anno_data,
                         config.start.unwrap_or(1),
                         config.end.unwrap_or(0),
                         &group,
-                    ){
+                    ) {
                         Ok(data) => data,
                         Err(err) => {
                             eprintln!("{} {}", message::INVALID_PATH_MSG, err);
                             std::process::exit(0);
                         }
                     }
-
-                   
+                    
                 } else {
                     eprintln!("{}", message::INVALID_PATH_MSG);
                     0
@@ -237,13 +237,13 @@ pub fn run(config: MDAOptions) -> Result<(), Box<dyn Error>> {
         let group = match config.group {
             Some(group) => {
                 if group == "NONE" {
-                    eprintln!("Please input group");
+                    eprintln!("Please input annotation group!");
                     std::process::exit(0);
                 }
                 group
             }
             None => {
-                eprintln!("Please input group");
+                eprintln!("Please input annotation group!");
                 std::process::exit(0);
             }
         };
@@ -257,102 +257,14 @@ pub fn run(config: MDAOptions) -> Result<(), Box<dyn Error>> {
                             eprintln!("Failed to read data from MDA file: {}", err);
                         }
                     }
-                } else {
-                    // do nothing
-                }
+                } 
             }
             _ => {
                 eprintln!("{}", message::INVALID_PATH_MSG);
                 std::process::exit(0);
             }
         }
-    }   else if config.action == "gen_combine" {
-        // Record start time
-        let start_time = record_start_time(&config.action);
-        // Generate mda files
-        let number_of_mda_files = match (&config.train, &config.anno_config, &config.output) {
-            (Some(train_data), Some(anno_config), Some(output)) => {
-                match generate_mda_combined_annotations(train_data, anno_config, output, &config) {
-                    Ok(data) => data,
-                    Err(err) => {
-                        eprintln!("{} {}", message::INVALID_PATH_MSG, err);
-                        std::process::exit(0);
-                    }
-                }
-            }
-            _ => {
-                eprintln!("{}", message::INVALID_PATH_MSG);
-                std::process::exit(0);
-            }
-        };
-        // Record end time
-        record_end_time(start_time, number_of_mda_files, "generated");
-    } else if config.action == "gen_one_one" {
-        // Record start time
-        let start_time = record_start_time(&config.action);
-
-        // Generate mda files
-        let number_of_mda_files = match (&config.train, &config.anno, &config.output) {
-            (Some(train_path), Some(anno_path), Some(output)) => {
-                if is_file(train_path) && is_file(anno_path) {
-                    generate_mda_separate_annotation_one_to_one(
-                        train_path, anno_path, output, &config,
-                    )?;
-                    1
-                } else if is_directory(train_path) && is_directory(anno_path) {
-                    match generate_mda_separate_annotation_one_to_one_in_folder(
-                        train_path, anno_path, output, &config,
-                    ) {
-                        Ok(data) => data,
-                        Err(err) => {
-                            eprintln!("{} {}", message::INVALID_PATH_MSG, err);
-                            std::process::exit(0);
-                        }
-                    }
-                } else {
-                    0
-                }
-            }
-            _ => {
-                eprintln!("{}", message::INVALID_PATH_MSG);
-                std::process::exit(0);
-            }
-        };
-        // Record end time
-        record_end_time(start_time, number_of_mda_files, "generated");
-    } else if config.action == "gen_one_many" {
-        // Record start time
-        let start_time = record_start_time(&config.action);
-
-        // Generate mda files
-        let number_of_mda_files = match (&config.train, &config.annos, &config.output) {
-            (Some(train_path), Some(anno_group), Some(output)) => {
-                if is_file(train_path) {
-                    generate_mda_separate_annotations_one_to_many(
-                        train_path, anno_group, output, &config,
-                    )?;
-                    1
-                } else if is_directory(train_path) {
-                    match generate_mda_separate_annotations_one_to_many_in_folder(
-                        train_path, anno_group, output, &config,
-                    ) {
-                        Ok(data) => data,
-                        Err(err) => {
-                            eprintln!("{} {}", message::INVALID_PATH_MSG, err);
-                            std::process::exit(0);
-                        }
-                    }
-                } else {
-                    0
-                }
-            }
-            _ => {
-                eprintln!("{}", message::INVALID_PATH_MSG);
-                std::process::exit(0);
-            }
-        };
-        // Record end time
-        record_end_time(start_time, number_of_mda_files, "generated");
+ 
     } else if config.action == "group" {
         match &config.mda {
             Some(mda) => match read_anno_groups_from_mda(mda) {
@@ -407,12 +319,18 @@ pub fn run(config: MDAOptions) -> Result<(), Box<dyn Error>> {
         let number_of_mda_files = match (&config.train, &config.anno, &config.mda) {
             // Extract anno data into different files
             (Some(train_data), Some(anno_data), Some(mda)) => {
+            
+
                 let anno_version: i32 = config.rev.unwrap_or_default();
                 if is_file(mda) && is_directory(train_data) && is_directory(anno_data) {
-                    extract_mda(mda, train_data, anno_data, anno_version, &format, &group)?;
+                   
+
+                     extract_mda(mda, train_data, anno_data, anno_version, &format, &group)?;
 
                     1
-                } else if is_directory(mda) {
+                } else if is_directory(mda) && is_directory(anno_data) {
+                  
+
                     extract_mda_more(
                         mda,
                         train_data,
@@ -423,70 +341,7 @@ pub fn run(config: MDAOptions) -> Result<(), Box<dyn Error>> {
                         threads,
                     )?;
                     1
-                } else if is_file(anno_data) && is_directory(mda) && is_directory(train_data) {
-                    // let anno_version: i32 = config.rev.unwrap_or_default();
-
-                    // let mut mda_files: Vec<String> = Vec::new();
-
-                    // find_mda_files_in_dir(Path::new(mda), &mut mda_files);
-                    // let pb = ProgressBar::new(mda_files.len() as u64);
-
-                    // let mut content = String::new();
-                    // for file in &mda_files {
-                    //     let train_data = train_data.to_string() + &extract_file_name(file);
-                    //     let anno = extract_data_from_mda_and_anno_in_one_file(
-                    //         file,
-                    //         &train_data,
-                    //         anno_version,
-                    //     );
-                    //     let (data_type, anno) = match anno {
-                    //         Ok(anno) => anno,
-                    //         Err(_) => {
-                    //             std::process::exit(0);
-                    //         }
-                    //     };
-                    //     let anno_content = anno.join(" ");
-                    //     let mut train_name = String::new();
-                    //     match data_type {
-                    //         DataType::Text => {
-                    //             let name = extract_file_name(&train_data);
-                    //             train_name = name + ".txt";
-                    //         }
-                    //         DataType::Image => {
-                    //             let name = extract_file_name(&train_data);
-                    //             train_name = name + ".jpg";
-                    //         }
-                    //         DataType::Video => {
-                    //             let name = extract_file_name(&train_data);
-                    //             train_name = name + ".mp4";
-                    //         }
-                    //         DataType::Audio => {
-                    //             let name = extract_file_name(&train_data);
-                    //             train_name = name + ".wav";
-                    //         }
-                    //     }
-                    //     let one_line = train_name + " " + &anno_content;
-
-                    //     content = content + &one_line + "\n";
-                    //     pb.inc(1);
-                    // }
-                    // pb.finish_with_message("done");
-                    // let mut file = match File::create(anno_data) {
-                    //     Ok(file) => file,
-                    //     Err(err) => {
-                    //         eprintln!("Error creating file: {}", err);
-                    //         std::process::exit(0);
-                    //     }
-                    // };
-
-                    // match file.write_all(content.as_bytes()) {
-                    //     Ok(_) => println!("\nFile write successful!"),
-                    //     Err(err) => eprintln!("Error writing to file: {}", err),
-                    // }
-
-                    // mda_files.len()
-                    0
-                } else {
+                }else {
                     eprintln!("{}", message::INVALID_PATH_MSG);
                     0
                 }
@@ -498,6 +353,99 @@ pub fn run(config: MDAOptions) -> Result<(), Box<dyn Error>> {
         };
         // Record end time
         record_end_time(start_time, number_of_mda_files, "extracted");
+    } else if config.action == "generate" {
+        // Record start time
+        let start_time = record_start_time(&config.action);
+
+        // Generate mda files
+        let mut number_of_mda_files = 0;
+        let mode = config.mode.as_ref().unwrap_or(&"NONE".to_string()).clone();
+        if mode == "combine" {
+            number_of_mda_files = match (&config.train, &config.anno_config, &config.output) {
+                (Some(train_data), Some(anno_config), Some(output)) => {
+                    match generate_mda_combined_annotations(
+                        train_data,
+                        anno_config,
+                        output,
+                        &config,
+                    ) {
+                        Ok(data) => data,
+                        Err(err) => {
+                            eprintln!("{} ERROR= {}", message::GENERATE_MSG, err);
+                            std::process::exit(0);
+                        }
+                    }
+                }
+                _ => {
+                    eprintln!("{}", message::INVALID_PATH_MSG);
+                    std::process::exit(0);
+                }
+            };
+        } else if mode == "one" {
+            number_of_mda_files = match (&config.train, &config.anno, &config.output) {
+                (Some(train_path), Some(anno_path), Some(output)) => {
+                    if is_file(train_path) && is_file(anno_path) {
+                        generate_mda_separate_annotation_one_to_one(
+                            train_path, anno_path, output, &config,
+                        )?;
+                        1
+                    } else if is_directory(train_path) && is_directory(anno_path) {
+                        match generate_mda_separate_annotation_one_to_one_in_folder(
+                            train_path, anno_path, output, &config,
+                        ) {
+                            Ok(data) => data,
+                            Err(err) => {
+                                eprintln!("{} ERROR= {}", message::GENERATE_MSG, err);
+                                std::process::exit(0);
+                            }
+                        }
+                    } else {
+                        0
+                    }
+                }
+                _ => {
+                    eprintln!("{} {}", message::GENERATE_MSG, message::INVALID_PATH_MSG);
+                    std::process::exit(0);
+                }
+            };
+        } else if mode == "multiple" {
+            number_of_mda_files = match (&config.train, &config.annos, &config.output) {
+                (Some(train_path), Some(anno_group), Some(output)) => {
+                    if is_file(train_path) {
+                        generate_mda_separate_annotations_one_to_many(
+                            train_path, anno_group, output, &config,
+                        )?;
+                        1
+                    } else if is_directory(train_path) {
+                        match generate_mda_separate_annotations_one_to_many_in_folder(
+                            train_path, anno_group, output, &config,
+                        ) {
+                            Ok(data) => data,
+                            Err(err) => {
+                                eprintln!("{} ERROR= {}", message::GENERATE_MSG, err);
+                                std::process::exit(0);
+                            }
+                        }
+                    } else {
+                        0
+                    }
+                }
+                _ => {
+                    eprintln!("{} {}", message::GENERATE_MSG, message::INVALID_PATH_MSG);
+                    std::process::exit(0);
+                }
+            };
+        } else {
+            eprintln!(
+                "{} Please input the correct generate mode!",
+                message::GENERATE_MSG,
+                
+            );
+            std::process::exit(0);
+        }
+
+        // Record end time
+        record_end_time(start_time, number_of_mda_files, "generated");
     } else {
         println!(
             "\x1b[38;5;208m[WARN]\x1b[0m Wrong action! Support 5 actions for MDA: generate, list, update, version, extract!\n- generate: generate mda files for data.\n- list: list basic info of mda files\n- update: update the annotation data in mda files\n- version: list all versions of mda files\n- extract: extract training data and annotation data from mda files"
