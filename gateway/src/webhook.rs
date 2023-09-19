7a9636821b00bae292ad4cde83b89b0acf7f8cea
Andres-Nju <1572378310@qq.com>
2023-09-19 14:17:21 +0800

    fix unused functions

--- gateway/src/webhook.rs
+++ gateway/src/webhook.rs
@@ -65,13 +65,13 @@ pub async fn webhook_server(options: &WebhookOptions) -> Result<(), Box<dyn std:
     // Read environment variables
     let github_app_id = env::var("GITHUB_APP_ID").expect("Missing GITHUB_APP_ID");
     let github_private_key = env::var("GITHUB_PRIVATE_KEY").expect("Missing GITHUB_PRIVATE_KEY");
-    let webhook_secret = env::var("GITHUB_WEBHOOK_SECRET").expect("Missing GITHUB_WEBHOOK_SECRET");
+    let _webhook_secret = env::var("GITHUB_WEBHOOK_SECRET").expect("Missing GITHUB_WEBHOOK_SECRET");
 
     // Create RSA private key from the provided environment variable
     let rsa_key = EncodingKey::from_rsa_pem(github_private_key.as_bytes())
         .expect("Failed to load private key");
     // Create Octocrab instance for GitHub App authentication
-    let octocrab = Octocrab::builder()
+    let _octocrab = Octocrab::builder()
         .app(AppId::from(github_app_id.parse::<u64>().unwrap()), rsa_key)
         .build()
         .expect("Failed to create Octocrab instance");
@@ -86,13 +86,11 @@ pub async fn webhook_server(options: &WebhookOptions) -> Result<(), Box<dyn std:
     } = options;
     let server_url = format!("{}:{}", host, port);
 
-    // let config =  LfsConfig::from(options.to_owned());
     let state = AppState {
         storage: database::init(data_source).await,
         options: options.to_owned(),
     };
     let app = Router::new()
-        //.nest("/", api_routers::routers(state.clone()))
         .route("/", post(post_method_router))
         .layer(ServiceBuilder::new().layer(CorsLayer::new().allow_origin(Any)))
         .with_state(state);
@@ -116,7 +114,7 @@ async fn post_method_router(
         "opened" => {
             state.storage.save_issue(issue_event.convert_to_model()).await.unwrap();
             let issue_ = state.storage.get_issue_by_id(issue_event.id()).await.unwrap().unwrap();
-            // println!("{:?}", issue_);
+            println!("{:?}", issue_);
         },
         "reopened" | 
         "closed" => {
@@ -139,68 +137,7 @@ async fn post_method_router(
     Ok(response)
 }
 
-mod api_routers {
-    use std::collections::HashMap;
 
-    use axum::{
-        extract::{Query, State},
-        response::IntoResponse,
-        routing::get,
-        Json, Router,
-    };
-    use hyper::StatusCode;
-
-    use crate::{
-        api_service::obj_service::ObjectService,
-        model::object_detail::{BlobObjects, TreeObjects},
-    };
-
-    use super::AppState;
-
-    pub fn routers<S>(state: AppState) -> Router<S> {
-        Router::new()
-            .route("/blob", get(get_blob_object))
-            .route("/tree", get(get_tree_objects))
-            .route("/object", get(get_origin_object))
-            .with_state(state)
-    }
-
-    async fn get_blob_object(
-        Query(query): Query<HashMap<String, String>>,
-        state: State<AppState>,
-    ) -> Result<Json<BlobObjects>, (StatusCode, String)> {
-        let repo_path = query.get("repo_path").unwrap();
-        let object_id = query.get("object_id").unwrap();
-        let object_service = ObjectService {
-            storage: state.storage.clone(),
-        };
-        object_service.get_blob_objects(object_id, repo_path).await
-    }
-
-    async fn get_tree_objects(
-        Query(query): Query<HashMap<String, String>>,
-        state: State<AppState>,
-    ) -> Result<Json<TreeObjects>, (StatusCode, String)> {
-        let object_id = query.get("object_id");
-        let repo_path = query.get("repo_path").unwrap();
-        let object_service = ObjectService {
-            storage: state.storage.clone(),
-        };
-        object_service.get_tree_objects(object_id, repo_path).await
-    }
-
-    async fn get_origin_object(
-        Query(query): Query<HashMap<String, String>>,
-        state: State<AppState>,
-    ) -> Result<impl IntoResponse, (StatusCode, String)> {
-        let repo_path = query.get("repo_path").unwrap();
-        let object_id = query.get("object_id").unwrap();
-        let object_service = ObjectService {
-            storage: state.storage.clone(),
-        };
-        object_service.get_objects_data(object_id, repo_path).await
-    }
-}
 
 #[cfg(test)]
 mod tests {}
