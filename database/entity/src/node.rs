054f17808f8b1e52c8f2993a1c7583abbce126c7
Xiaoyang Han <lux1an@qq.com>
2023-09-23 21:31:28 +0800

    (database) Add sorting related traits and storage interfaces to the node model
    Signed-off-by: Xiaoyang Han <lux1an@qq.com>

--- database/entity/src/node.rs
+++ database/entity/src/node.rs
@@ -26,3 +26,115 @@ pub struct Model {
 pub enum Relation {}
 
 impl ActiveModelBehavior for ActiveModel {}
+
+use std::cmp::Ordering;
+
+impl PartialOrd for Model {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let node_type_order = match (self.node_type.as_str(), other.node_type.as_str()) {
+            ("commit", "commit") | ("tree", "tree") | ("blob", "blob") | ("tag", "tag") => {
+                Ordering::Equal
+            }
+            ("commit", _) => Ordering::Less,
+            ("tree", "commit") => Ordering::Greater,
+            ("tree", _) => Ordering::Less,
+            ("blob", "commit") | ("blob", "tree") => Ordering::Greater,
+            ("blob", _) => Ordering::Less,
+            ("tag", _) => Ordering::Greater,
+            (&_, _) => panic!("unknow types in ordering git nodes ")
+            
+        };
+
+        if node_type_order != Ordering::Equal {
+            Some(node_type_order)
+        } else {
+            let full_path_order = self.full_path.cmp(&other.full_path);
+            if full_path_order != Ordering::Equal {
+                Some(full_path_order)
+            } else {
+                Some(self.size.cmp(&other.size))
+            }
+        }
+    }
+}
+
+impl Ord for Model {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests{
+   
+    use super::Model;
+    #[test]
+    fn test_nodes_sort(){
+         // 示例使用
+    let model1 = Model {
+        id: 1,
+        node_id: 1,
+        git_id: "id1".to_string(),
+        node_type: "blob".to_string(),
+        name: Some("Name1".to_string()),
+        mode: vec![],
+        content_sha: None,
+        size: 10,
+        repo_path: "path1".to_string(),
+        full_path: "fullpath1".to_string(),
+        created_at: chrono::Utc::now().naive_utc(),
+        updated_at: chrono::Utc::now().naive_utc(),
+        last_commit: "no commit".to_string(),
+    };
+
+    let model2 = Model {
+        id: 2,
+        node_id: 2,
+        git_id: "id2".to_string(),
+        node_type: "tree".to_string(),
+        name: Some("Name2".to_string()),
+        mode: vec![],
+        content_sha: None,
+        size: 20,
+        repo_path: "path2".to_string(),
+        full_path: "fullpath2".to_string(),
+        created_at: chrono::Utc::now().naive_utc(),
+        updated_at: chrono::Utc::now().naive_utc(),
+        last_commit: "no commit".to_string(),
+    };
+    let model3 = Model {
+        id: 3,
+        node_id: 3,
+        git_id: "id2".to_string(),
+        node_type: "tree".to_string(),
+        name: Some("Name2".to_string()),
+        mode: vec![],
+        content_sha: None,
+        size: 30,
+        repo_path: "path2".to_string(),
+        full_path: "fullpath1".to_string(),
+        created_at: chrono::Utc::now().naive_utc(),
+        updated_at: chrono::Utc::now().naive_utc(),
+        last_commit: "no commit".to_string(),
+    };
+    let model4 = Model {
+        id: 4,
+        node_id: 4,
+        git_id: "id2".to_string(),
+        node_type: "tree".to_string(),
+        name: Some("Name2".to_string()),
+        mode: vec![],
+        content_sha: None,
+        size: 40,
+        repo_path: "path2".to_string(),
+        full_path: "fullpath2".to_string(),
+        created_at: chrono::Utc::now().naive_utc(),
+        updated_at: chrono::Utc::now().naive_utc(),
+        last_commit: "no commit".to_string(),
+    };
+
+    let mut nodes = vec![model1,model2,model3,model4];
+    nodes.sort();
+    print!("{:?}", nodes);
+    }
+}
