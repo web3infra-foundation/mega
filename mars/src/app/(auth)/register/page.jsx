4ca5b713c3262222dfd5db792a072592a67fe35b
Eli Ma <eli@patch.sh>
2023-11-20 16:27:48 +0800

    Refactoring the moon for UI, mars for website
    Signed-off-by: Eli Ma <eli@patch.sh>

---
+++ mars/src/app/(auth)/register/page.jsx
@@ -0,0 +1,74 @@
+import Link from 'next/link'
+
+import { AuthLayout } from '@/components/AuthLayout'
+import { Button } from '@/components/Button'
+import { SelectField, TextField } from '@/components/Fields'
+
+export const metadata = {
+  title: 'Sign Up',
+}
+
+export default function Register() {
+  return (
+    <AuthLayout
+      title="Sign up for an account"
+      subtitle={
+        <>
+          Already registered?{' '}
+          <Link href="/login" className="text-cyan-600">
+            Sign in
+          </Link>{' '}
+          to your account.
+        </>
+      }
+    >
+      <form>
+        <div className="grid grid-cols-2 gap-6">
+          <TextField
+            label="First name"
+            name="first_name"
+            type="text"
+            autoComplete="given-name"
+            required
+          />
+          <TextField
+            label="Last name"
+            name="last_name"
+            type="text"
+            autoComplete="family-name"
+            required
+          />
+          <TextField
+            className="col-span-full"
+            label="Email address"
+            name="email"
+            type="email"
+            autoComplete="email"
+            required
+          />
+          <TextField
+            className="col-span-full"
+            label="Password"
+            name="password"
+            type="password"
+            autoComplete="new-password"
+            required
+          />
+          <SelectField
+            className="col-span-full"
+            label="How did you hear about us?"
+            name="referral_source"
+          >
+            <option>AltaVista search</option>
+            <option>Super Bowl commercial</option>
+            <option>Our route 34 city bus ad</option>
+            <option>The “Never Use This” podcast</option>
+          </SelectField>
+        </div>
+        <Button type="submit" color="cyan" className="mt-8 w-full">
+          Get started today
+        </Button>
+      </form>
+    </AuthLayout>
+  )
+}
---
+++ mars/src/app/layout.jsx
@@ -0,0 +1,32 @@
+import { Inter } from 'next/font/google'
+import clsx from 'clsx'
+
+import '@/styles/tailwind.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+export const metadata = {
+  title: {
+    template: '%s - Pocket',
+    default: 'Pocket - Invest at the perfect time.',
+  },
+  description:
+    'By leveraging insights from our network of industry insiders, you’ll know exactly when to buy to maximize profit, and exactly when to sell to avoid painful losses.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={clsx('h-full bg-gray-50 antialiased', inter.variable)}
+    >
+      <body className="flex h-full flex-col">
+        <div className="flex min-h-full flex-col">{children}</div>
+      </body>
+    </html>
+  )
+}
