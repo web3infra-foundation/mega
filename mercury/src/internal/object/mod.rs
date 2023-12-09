902174f4f3bd4faeeb28ebf758479b551e212611
Quanyi Ma <eli@patch.sh>
2023-12-09 21:46:56 +0800

    Add object type in pack decode func #238
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- mercury/src/internal/object/mod.rs
+++ mercury/src/internal/object/mod.rs
@@ -6,22 +6,9 @@
 pub mod types;
 pub mod blob;
 
-use std::io::{Read, Write};
+use std::io::Read;
 use std::fmt::Display;
 
-
-/// Implementation of the `Object` trait.
-///
-/// The naming conventions for the methods in this implementation are designed to be intuitive and self-explanatory:
-///
-/// 1. `new` Prefix: 
-///
-/// 2. `from` Prefix:
-///
-/// 3. `to` Prefix:
-///
-/// These method naming conventions (`new`, `from`, `to`) provide clarity and predictability in the API, making it easier for users 
-/// to understand the intended use and functionality of each method within the `SHA1` struct.
-pub trait ObjectTrait: Read + Write + Send + Sync + Display {
-    fn new(data: impl Read) -> Self;
+pub trait ObjectTrait: Send + Sync + Display {
+    fn new(&self, data: impl Read) -> Self where Self: Sized;
 }
