786bea29891d1f5aa672c0e1142593931be7b957
Quanyi Ma <eli@patch.sh>
2023-12-10 21:04:14 +0800

    Add from_bytes for blob #238
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- mercury/src/internal/object/mod.rs
+++ mercury/src/internal/object/mod.rs
@@ -6,9 +6,12 @@
 pub mod types;
 pub mod blob;
 
-use std::io::Read;
 use std::fmt::Display;
 
+use crate::internal::object::types::ObjectType;
+
 pub trait ObjectTrait: Send + Sync + Display {
-    fn new(&self, data: impl Read) -> Self where Self: Sized;
+    fn from_bytes(data: Vec<u8>) -> Self where Self: Sized;
+
+    fn get_type(&self) -> ObjectType;
 }
