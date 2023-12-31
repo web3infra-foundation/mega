beadad04c863f72d3cde024ea43022e4fb593991
Quanyi Ma <eli@patch.sh>
2023-12-31 21:45:00 +0800

    Add CacheOjbect and Caches in decode pack file
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- mercury/src/internal/object/mod.rs
+++ mercury/src/internal/object/mod.rs
@@ -4,20 +4,21 @@
 //! 
 //! 
 pub mod types;
+pub mod utils;
 pub mod blob;
 
 use std::fmt::Display;
 
-use crate::hash::SHA1;
+use crate::errors::GitError;
 use crate::internal::object::types::ObjectType;
 
-pub trait ObjectTrait: Send + Sync + Display {
+pub trait ObjectTrait: Send + Sync + Display{
     /// Creates a new object from a byte slice.
-    fn from_bytes(data: Vec<u8>) -> Self where Self: Sized;
-
-    /// Creates a new object from a byte slice with a given ID.
-    fn from_bytes_with_id(data: Vec<u8>, id: SHA1) -> Self where Self: Sized;
+    fn from_bytes(data: Vec<u8>) ->  Result<Self, GitError> where Self: Sized;
 
     /// Returns the type of the object.
     fn get_type(&self) -> ObjectType;
+
+    ///
+    fn get_size(&self) -> usize;
 }
--- mercury/src/internal/pack/cache.rs
+++ mercury/src/internal/pack/cache.rs
@@ -5,7 +5,41 @@
 //! 
 //! 
 
+use std::collections::HashMap;
 
-pub trait CacheTrait {
-    type T ;
+use crate::hash::SHA1;
+use crate::internal::object::types::ObjectType;
+use crate::internal::object::ObjectTrait;
+
+#[allow(unused)]
+#[derive(Debug, Clone)]
+pub struct CacheObject {
+    pub delta_offset: usize,
+    pub delta_ref: SHA1,
+    pub data_decompress: Vec<u8>,
+    pub object_type: ObjectType,
+    pub offset: usize,
+}
+
+#[allow(unused)]
+pub struct Caches {
+    pub objects: Vec<Box<dyn ObjectTrait>>,
+    pub map_offset: HashMap<usize, CacheObject>,
+}
+
+
+impl CacheObject {
+    
+}
+
+
+impl Caches {
+
+    ///
+    /// 
+    /// 
+    /// 
+    pub fn insert(&mut self, offset: usize, object: CacheObject) {
+        self.map_offset.insert(offset, object);
+    }
 }
