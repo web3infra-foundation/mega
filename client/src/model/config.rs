ff183022d815afd855ef6dc9f14e1508bb32cc18
Qihang Cai <mrbeanc@qq.com>
2024-03-31 23:58:52 +0800

    rename `config_entry` to `config`
    Signed-off-by: Qihang Cai <mrbeanc@qq.com>

--- client/src/model/config.rs
+++ client/src/model/config.rs
@@ -1,7 +1,7 @@
 use sea_orm::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
-#[sea_orm(table_name = "config_entry")]
+#[sea_orm(table_name = "config")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = true)]
     pub id: i64,
