9093f86f236a04846b2dbf2a4ee29a5062d3bf95
Hou Xiaoxuan <Hou_Xiaoxuan@163.com>
2024-04-17 16:06:12 +0800

    fix: clippy
    Signed-off-by: Hou Xiaoxuan <Hou_Xiaoxuan@163.com>

--- libra/src/utils/util.rs
+++ libra/src/utils/util.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 pub const ROOT_DIR: &str = ".libra";
 pub const DATABASE: &str = "libra.db";
 
+#[allow(dead_code)]
 pub fn cur_dir() -> PathBuf {
     env::current_dir().unwrap()
 }
