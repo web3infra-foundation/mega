bd69c40ba4595005744f71f8295fe952104c14b3
Hou Xiaoxuan <Hou_Xiaoxuan@163.com>
2024-05-05 20:03:37 +0800

    fix `discovery_reference`
    Signed-off-by: Hou Xiaoxuan <Hou_Xiaoxuan@163.com>

--- libra/src/internal/protocel/mod.rs
+++ libra/src/internal/protocel/mod.rs
@@ -1,8 +1,24 @@
 use url::Url;
 
 pub mod https_client;
-
 pub trait ProtocolClient {
     /// create client from url
     fn from_url(url: &Url) -> Self;
+}
+
+#[cfg(test)]
+mod test {
+
+    pub fn init_debug_loger() {
+        tracing::subscriber::set_global_default(
+            tracing_subscriber::fmt()
+                .with_max_level(tracing::Level::DEBUG)
+                .finish(),
+        )
+        .unwrap();
+    }
+
+    pub fn init_loger() {
+        tracing_subscriber::fmt().init();
+    }
 }
