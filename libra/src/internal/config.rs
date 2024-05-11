393c4d42bc0ead213d62a6cf13ec9dbc3ddff444
Qihang Cai <mrbeanc@qq.com>
2024-05-11 17:06:46 +0800

    improve: add Global db connection (singleton) for performance
    Signed-off-by: Qihang Cai <mrbeanc@qq.com>

--- libra/src/internal/config.rs
+++ libra/src/internal/config.rs
@@ -1,18 +1,10 @@
 use std::collections::HashSet;
 
-use crate::internal::db;
-use crate::internal::model::config;
+use sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter};
 use sea_orm::ActiveValue::Set;
-use sea_orm::{ActiveModelTrait, ColumnTrait, DbConn, EntityTrait, QueryFilter};
-use tokio::sync::OnceCell;
 
-// singleton pattern
-static DB_CONN: OnceCell<DbConn> = OnceCell::const_new();
-async fn get_db_conn() -> &'static DbConn {
-    DB_CONN
-        .get_or_init(|| async { db::get_db_conn().await.unwrap() })
-        .await
-}
+use crate::internal::db::get_db_conn_instance;
+use crate::internal::model::config;
 
 pub struct Config;
 
@@ -24,7 +16,7 @@ pub struct RemoteConfig {
 impl Config {
     // todo accept a db connect or a transaction from outside
     pub async fn insert(configuration: &str, name: Option<&str>, key: &str, value: &str) {
-        let db = get_db_conn().await;
+        let db = get_db_conn_instance().await;
         let config = config::ActiveModel {
             configuration: Set(configuration.to_owned()),
             name: Set(name.map(|s| s.to_owned())),
@@ -36,7 +28,7 @@ impl Config {
     }
 
     pub async fn remote_configs() -> Vec<RemoteConfig> {
-        let db = get_db_conn().await;
+        let db = get_db_conn_instance().await;
         let remotes = config::Entity::find()
             .filter(config::Column::Configuration.eq("remote"))
             .all(db)
