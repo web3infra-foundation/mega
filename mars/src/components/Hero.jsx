51149ec2997cfe0097979e741e6d5ba2a1d2cf5a
Quanyi Ma <eli@patch.sh>
2024-05-14 22:18:11 +0800

    Clean components of template in the website page
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- mars/src/components/Hero.jsx
+++ mars/src/components/Hero.jsx
@@ -105,15 +105,15 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-              Invest at the perfect time.
+              Redefining Open Source with Monolithic and Decentralized
             </h1>
             <p className="mt-6 text-lg text-gray-600">
-              By leveraging insights from our network of industry insiders,
-              you’ll know exactly when to buy to maximize profit, and exactly
-              when to sell to avoid painful losses.
+              Mega is an unofficial open source implementation of Google Piper. It is a monorepo & monolithic codebase
+              management system that supports Git. Mega is designed to manage large-scale codebases,
+              streamline development, and foster collaboration.
             </p>
             <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
-              <AppStoreLink />
+              {/*<AppStoreLink />*/}
               <Button
                 href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
                 variant="outline"
@@ -123,38 +123,38 @@ export function Hero() {
               </Button>
             </div>
           </div>
-          <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
-            <BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
-            <div className="-mx-4 h-[448px] px-9 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">
-              <PhoneFrame className="mx-auto max-w-[366px]" priority>
-                <AppDemo />
-              </PhoneFrame>
-            </div>
-          </div>
-          <div className="relative -mt-4 lg:col-span-7 lg:mt-0 xl:col-span-6">
-            <p className="text-center text-sm font-semibold text-gray-900 lg:text-left">
-              As featured in
-            </p>
-            <ul
-              role="list"
-              className="mx-auto mt-8 flex max-w-xl flex-wrap justify-center gap-x-10 gap-y-8 lg:mx-0 lg:justify-start"
-            >
-              {[
-                ['Forbes', logoForbes],
-                ['TechCrunch', logoTechcrunch],
-                ['Wired', logoWired],
-                ['CNN', logoCnn, 'hidden xl:block'],
-                ['BBC', logoBbc],
-                ['CBS', logoCbs],
-                ['Fast Company', logoFastCompany],
-                ['HuffPost', logoHuffpost, 'hidden xl:block'],
-              ].map(([name, logo, className]) => (
-                <li key={name} className={clsx('flex', className)}>
-                  <Image src={logo} alt={name} className="h-8" unoptimized />
-                </li>
-              ))}
-            </ul>
-          </div>
+          {/*<div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">*/}
+          {/*  <BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />*/}
+          {/*  <div className="-mx-4 h-[448px] px-9 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">*/}
+          {/*    <PhoneFrame className="mx-auto max-w-[366px]" priority>*/}
+          {/*      <AppDemo />*/}
+          {/*    </PhoneFrame>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
+          {/*<div className="relative -mt-4 lg:col-span-7 lg:mt-0 xl:col-span-6">*/}
+          {/*  <p className="text-center text-sm font-semibold text-gray-900 lg:text-left">*/}
+          {/*    As featured in*/}
+          {/*  </p>*/}
+          {/*  <ul*/}
+          {/*    role="list"*/}
+          {/*    className="mx-auto mt-8 flex max-w-xl flex-wrap justify-center gap-x-10 gap-y-8 lg:mx-0 lg:justify-start"*/}
+          {/*  >*/}
+          {/*    {[*/}
+          {/*      ['Forbes', logoForbes],*/}
+          {/*      ['TechCrunch', logoTechcrunch],*/}
+          {/*      ['Wired', logoWired],*/}
+          {/*      ['CNN', logoCnn, 'hidden xl:block'],*/}
+          {/*      ['BBC', logoBbc],*/}
+          {/*      ['CBS', logoCbs],*/}
+          {/*      ['Fast Company', logoFastCompany],*/}
+          {/*      ['HuffPost', logoHuffpost, 'hidden xl:block'],*/}
+          {/*    ].map(([name, logo, className]) => (*/}
+          {/*      <li key={name} className={clsx('flex', className)}>*/}
+          {/*        <Image src={logo} alt={name} className="h-8" unoptimized />*/}
+          {/*      </li>*/}
+          {/*    ))}*/}
+          {/*  </ul>*/}
+          {/*</div>*/}
         </div>
       </Container>
     </div>
