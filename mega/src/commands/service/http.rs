6b19e2e34b2d4cdace2bdab21fcdea3b0941811d
benjamin.747 <benjamin.747@outlook.com>
2024-05-17 16:44:48 +0800

    rename subcommand `start` to `multi`, seperate `http` and `https` command

--- mega/src/commands/service/http.rs
+++ mega/src/commands/service/http.rs
@@ -1,20 +1,20 @@
 use clap::{ArgMatches, Args, Command, FromArgMatches};
 
 use common::{config::Config, errors::MegaResult};
-use gateway::https_server::{self, HttpsOptions};
+use gateway::https_server::{self, HttpOptions};
 
 
 pub fn cli() -> Command {
-    HttpsOptions::augment_args_for_update(Command::new("https").about("Start Mega HTTPS server"))
+    HttpOptions::augment_args_for_update(Command::new("http").about("Start Mega HTTP server"))
 }
 
 pub(crate) async fn exec(config: Config, args: &ArgMatches) -> MegaResult {
-    let server_matchers = HttpsOptions::from_arg_matches(args)
+    let server_matchers = HttpOptions::from_arg_matches(args)
         .map_err(|err| err.exit())
         .unwrap();
 
     tracing::info!("{server_matchers:#?}");
-    https_server::https_server(config, server_matchers).await;
+    https_server::http_server(config, server_matchers).await;
     Ok(())
 }
 
