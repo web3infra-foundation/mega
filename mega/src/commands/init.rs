6d473fa5a0350b71e86cb96b011f495b2fae57fb
Eli Ma <eli@patch.sh>
2024-05-18 23:45:50 +0800

    Fix clippy warnings
    Signed-off-by: Eli Ma <eli@patch.sh>

--- mega/src/commands/init.rs
+++ mega/src/commands/init.rs
@@ -1,5 +1,5 @@
-//!
-//!
+//! This module is responsible for handling the 'init' command.
+//! It initializes the mega monorepo structure.
 //!
 //!
 
@@ -8,10 +8,13 @@ use clap::{ArgMatches, Command};
 use common::{config::Config, errors::MegaResult};
 use gateway::init::init_monorepo;
 
+// This function generates the CLI for the 'init' command.
 pub fn cli() -> Command {
     Command::new("init").about("Initialize the mega monorepo structure")
 }
 
+// This function executes the 'init' command.
+// It calls the `init_monorepo` function from the `gateway` module.
 #[tokio::main]
 pub(crate) async fn exec(config: Config, _: &ArgMatches) -> MegaResult {
     init_monorepo(config).await.unwrap();
--- mega/src/commands/service/mod.rs
+++ mega/src/commands/service/mod.rs
@@ -1,9 +1,8 @@
+//! This module is responsible for handling the 'service' command.
+//! It includes subcommands for starting different kinds of servers, such as HTTPS and SSH.
 //!
 //!
 //!
-//!
-//!
-
 use clap::{ArgMatches, Command};
 
 use common::{config::Config, errors::MegaResult};
@@ -12,6 +11,8 @@ mod https;
 mod ssh;
 mod start;
 
+// This function generates the CLI for the 'service' command.
+// It includes subcommands for each server type.
 pub fn cli() -> Command {
     let subcommands = vec![https::cli(), ssh::cli(), start::cli()];
     Command::new("service")
@@ -19,6 +20,8 @@ pub fn cli() -> Command {
         .subcommands(subcommands)
 }
 
+// This function executes the 'service' command.
+// It determines which subcommand was used and calls the appropriate function.
 #[tokio::main]
 pub(crate) async fn exec(config: Config, args: &ArgMatches) -> MegaResult {
     let (cmd, subcommand_args) = match args.subcommand() {
