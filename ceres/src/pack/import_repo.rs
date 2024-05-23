208b10e3648ec0d9b14c36a39831ed9dda6a47da
benjamin.747 <benjamin.747@outlook.com>
2024-05-23 16:52:23 +0800

    fix bug: incremental_pack repeated count obj num

--- ceres/src/pack/import_repo.rs
+++ ceres/src/pack/import_repo.rs
@@ -1,3 +1,5 @@
+use std::{collections::{HashMap, HashSet}, str::FromStr, sync::atomic::{AtomicUsize, Ordering}};
+
 use async_trait::async_trait;
 use bytes::Bytes;
 use tokio::sync::mpsc;
@@ -9,7 +11,7 @@ use jupiter::{
     context::Context,
     storage::{batch_query_by_columns, GitStorageProvider},
 };
-use mercury::internal::pack::encode::PackEncoder;
+use mercury::{hash::SHA1, internal::pack::encode::PackEncoder};
 use mercury::{
     errors::GitError,
     internal::{
@@ -119,10 +121,95 @@ impl PackHandler for ImportRepo {
 
     async fn incremental_pack(
         &self,
-        _want: Vec<String>,
-        _have: Vec<String>,
+        mut want: Vec<String>,
+        have: Vec<String>,
     ) -> Result<ReceiverStream<Vec<u8>>, GitError> {
-        unimplemented!()
+        let pack_config = &self.context.config.pack;
+        let storage = self.context.services.git_db_storage.clone();
+        let obj_num = AtomicUsize::new(0);
+
+        let mut exist_objs = HashSet::new();
+
+        let mut want_commits: Vec<Commit> = storage
+            .get_commits_by_hashes(&self.repo, &want)
+            .await
+            .unwrap()
+            .into_iter()
+            .map(|x| x.into())
+            .collect();
+        let mut traversal_list: Vec<Commit> = want_commits.clone();
+
+        // traverse commit's all parents to find the commit that client does not have
+        while let Some(temp) = traversal_list.pop() {
+            for p_commit_id in temp.parent_commit_ids {
+                let p_commit_id = p_commit_id.to_plain_str();
+
+                if !have.contains(&p_commit_id) && !want.contains(&p_commit_id) {
+                    let parent: Commit = storage
+                        .get_commit_by_hash(&self.repo, &p_commit_id)
+                        .await
+                        .unwrap()
+                        .unwrap()
+                        .into();
+                    want_commits.push(parent.clone());
+                    want.push(p_commit_id);
+                    traversal_list.push(parent);
+                }
+            }
+        }
+
+        let want_tree_ids = want_commits
+            .iter()
+            .map(|c| c.tree_id.to_plain_str())
+            .collect();
+        let want_trees: HashMap<SHA1, Tree> = storage
+            .get_trees_by_hashes(&self.repo, want_tree_ids)
+            .await
+            .unwrap()
+            .into_iter()
+            .map(|m| (SHA1::from_str(&m.tree_id).unwrap(), m.into()))
+            .collect();
+
+        obj_num.fetch_add(want_commits.len(), Ordering::SeqCst);
+
+        let have_commits = storage.get_commits_by_hashes(&self.repo, &have).await.unwrap();
+        let have_trees = storage
+            .get_trees_by_hashes(&self.repo, have_commits.iter().map(|x| x.tree.clone()).collect())
+            .await
+            .unwrap();
+        // traverse to get exist_objs
+        for have_tree in have_trees {
+            self.traverse(have_tree.into(), &mut exist_objs, None).await;
+        }
+
+        let mut counted_obj = HashSet::new();
+        // traverse for get obj nums
+        for c in want_commits.clone() {
+            self.traverse_for_count(
+                want_trees.get(&c.tree_id).unwrap().clone(),
+                &exist_objs,
+                &mut counted_obj,
+                &obj_num,
+            )
+            .await;
+        }
+        let (entry_tx, entry_rx) = mpsc::channel(pack_config.channel_message_size);
+        let (stream_tx, stream_rx) = mpsc::channel(pack_config.channel_message_size);
+        let encoder = PackEncoder::new(obj_num.into_inner(), 0, stream_tx);
+        encoder.encode_async(entry_rx).await.unwrap();
+
+        for c in want_commits {
+            self.traverse(
+                want_trees.get(&c.tree_id).unwrap().clone(),
+                &mut exist_objs,
+                Some(&entry_tx),
+            )
+            .await;
+            entry_tx.send(c.into()).await.unwrap();
+        }
+        drop(entry_tx);
+
+        Ok(ReceiverStream::new(stream_rx))
     }
 
     async fn get_trees_by_hashes(&self, hashes: Vec<String>) -> Result<Vec<Tree>, MegaError> {
