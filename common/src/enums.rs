cf5c05cee9c105a104b80e7d6d886ba91928dfbe
wujian <353981613@qq.com>
2024-06-04 10:46:47 +0800

    Refactoring the relay startup method

--- common/src/enums.rs
+++ common/src/enums.rs
@@ -5,3 +5,21 @@ pub enum DataSource {
     Mysql,
     Postgres,
 }
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+pub enum ZtmType {
+    Agent,
+    Relay,
+}
+
+impl std::str::FromStr for ZtmType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "agent" => Ok(ZtmType::Agent),
+            "relay" => Ok(ZtmType::Relay),
+            _ => Err(format!("'{}' is not a valid ztm type", s)),
+        }
+    }
+}
