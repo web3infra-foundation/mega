2da6053c72b032ba9afcb70e655bbd0f11c1893f
Eli Ma <eli@patch.sh>
2024-06-09 13:52:57 +0800

    Update the .devcontainner
    Signed-off-by: Eli Ma <eli@patch.sh>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,8 +10,11 @@ apt-get install -y \
   vim \
   build-essential \
   openssl \
+  libssl-dev \
   fuse3 \
-  libfuse3-dev
+  libfuse3-dev \
+  pkg-config \
+  postgresql
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
@@ -22,9 +25,12 @@ rustup component add clippy
 cargo install cargo-expand
 cargo install cargo-edit
 
-## setup and install oh-my-zsh
+## Setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+
+## Start PostgreSQL
+/etc/init.d/postgresql start
