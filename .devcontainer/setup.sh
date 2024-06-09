9c6f55c2292db8346df0908830d8b43c0accf275
Eli Ma <eli@patch.sh>
2024-06-09 15:03:00 +0800

    Update PostgreSQL in devcontainer
    Signed-off-by: Eli Ma <eli@patch.sh>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -35,11 +35,11 @@ chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 ## Start PostgreSQL
 /etc/init.d/postgresql start
 
-sudo -u postgres psql mega -c "CREATE DATABASE mega;"
-sudo -u postgres psql mega < /workspaces/mega/sql/pg_20240205__init.sql
-sudo -u postgres psql mega -c "CREATE USER mega WITH ENCRYPTED PASSWORD 'mega';"
-sudo -u postgres psql mega -c "GRANT ALL PRIVILEGES ON DATABASE mega TO mega;"
-sudo -u postgres psql mega -c "GRANT ALL ON ALL TABLES IN SCHEMA public to mega;"
-sudo -u postgres psql mega -c "GRANT ALL ON ALL TABLES IN SCHEMA public to mega;"
-sudo -u postgres psql mega -c "GRANT ALL ON ALL SEQUENCES IN SCHEMA public to mega;"
-sudo -u postgres psql mega -c "GRANT ALL ON ALL FUNCTIONS IN SCHEMA public to mega;"
+#sudo -u postgres psql mega -c "CREATE DATABASE mega;"
+#sudo -u postgres psql mega < /workspaces/mega/sql/pg_20240205__init.sql
+#sudo -u postgres psql mega -c "CREATE USER mega WITH ENCRYPTED PASSWORD 'mega';"
+#sudo -u postgres psql mega -c "GRANT ALL PRIVILEGES ON DATABASE mega TO mega;"
+#sudo -u postgres psql mega -c "GRANT ALL ON ALL TABLES IN SCHEMA public to mega;"
+#sudo -u postgres psql mega -c "GRANT ALL ON ALL TABLES IN SCHEMA public to mega;"
+#sudo -u postgres psql mega -c "GRANT ALL ON ALL SEQUENCES IN SCHEMA public to mega;"
+#sudo -u postgres psql mega -c "GRANT ALL ON ALL FUNCTIONS IN SCHEMA public to mega;"
