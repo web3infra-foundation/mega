cd9e4944b890596f3f3bcc2eabbf973b3ec04b23
Eli Ma <eli@patch.sh>
2024-06-09 14:47:56 +0800

    Install PostgreSQL in devcontainer
    Signed-off-by: Eli Ma <eli@patch.sh>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -14,6 +14,7 @@ apt-get install -y \
   fuse3 \
   libfuse3-dev \
   pkg-config \
+  postgresql
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
@@ -30,3 +31,15 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+
+## Start PostgreSQL
+/etc/init.d/postgresql start
+
+sudo -u postgres psql mega -c "CREATE DATABASE mega;"
+sudo -u postgres psql mega < /workspaces/mega/sql/pg_20240205__init.sql
+sudo -u postgres psql mega -c "CREATE USER mega WITH ENCRYPTED PASSWORD 'mega';"
+sudo -u postgres psql mega -c "GRANT ALL PRIVILEGES ON DATABASE mega TO mega;"
+sudo -u postgres psql mega -c "GRANT ALL ON ALL TABLES IN SCHEMA public to mega;"
+sudo -u postgres psql mega -c "GRANT ALL ON ALL TABLES IN SCHEMA public to mega;"
+sudo -u postgres psql mega -c "GRANT ALL ON ALL SEQUENCES IN SCHEMA public to mega;"
+sudo -u postgres psql mega -c "GRANT ALL ON ALL FUNCTIONS IN SCHEMA public to mega;"
