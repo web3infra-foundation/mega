d714e3ab0f99662b175ce78ad4f1e25b76254325
Qihang Cai <mrbeanc@qq.com>
2024-06-17 15:49:54 +0800

    move `nostr gen` to `nostr.rs`
    Signed-off-by: Qihang Cai <mrbeanc@qq.com>

--- relay/src/pki.rs
+++ relay/src/pki.rs
@@ -10,8 +10,6 @@ use openssl::x509::X509;
 use rusty_vault::core::Core;
 use serde_json::{json, Value};
 
-use secp256k1::{rand, SecretKey};
-
 use super::vault::{CoreInfo, CORE, read_api, write_api};
 
 const ROLE: &str = "test";
@@ -151,7 +149,6 @@ pub fn verify_cert(cert_pem: &[u8]) -> bool {
 
 #[cfg(test)]
 mod tests {
-    use secp256k1::Secp256k1;
     use super::*;
 
     #[test]
@@ -176,16 +173,6 @@ mod tests {
 
         assert!(verify_cert(cert_pem.as_ref()));
     }
-
-    #[test]
-    fn test_nostr() {
-        let secp = Secp256k1::new();
-        let secret_key = SecretKey::new(&mut rand::thread_rng());
-        let public_key = secret_key.public_key(&secp);
-        // println!("secret_key: {}", secret_key.display_secret());
-        println!("{}", bs58::encode(public_key.serialize()).into_string());
-    }
-
 }
 
 #[cfg(test)]
