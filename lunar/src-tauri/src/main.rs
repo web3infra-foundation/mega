dde99b152d35e855b73fcb4de2ecc9bae7357a12
Hou Xiaoxuan <Hou_Xiaoxuan@163.com>
2024-07-17 20:32:27 +0800

    invoke tauri function
    Signed-off-by: Hou Xiaoxuan <Hou_Xiaoxuan@163.com>

--- lunar/src-tauri/src/main.rs
+++ lunar/src-tauri/src/main.rs
@@ -1,8 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn hello_string(name: &str) -> String {
+    format!("Hello from Rust, {}!", name)
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![hello_string])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
