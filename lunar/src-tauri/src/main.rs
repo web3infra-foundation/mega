bd971454c64e72636a586e88150428c7cddc0465
HouXiaoxuan <Hou_Xiaoxuan@163.com>
2024-07-22 18:54:43 +0800

    lunar tauri: use default args to start emga
    Signed-off-by: HouXiaoxuan <Hou_Xiaoxuan@163.com>

--- lunar/src-tauri/src/main.rs
+++ lunar/src-tauri/src/main.rs
@@ -6,8 +6,8 @@ fn hello_string(name: &str) -> String {
     format!("Hello from Rust, {}!", name)
 }
 
-fn start_mega(config_path: &str) {
-    let args_str = format!("-c \"{}\" service http", config_path);
+fn start_mega() {
+    let args_str = "service http".to_string();
     let args = args_str.split(' ').collect();
     mega::cli::parse(Some(args)).expect("failed to start mega");
 }
@@ -15,13 +15,9 @@ fn start_mega(config_path: &str) {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![hello_string])
-        .setup(|app| {
-            let resource_path = app
-                .path_resolver()
-                .resolve_resource("config.toml")
-                .expect("failed to resolve config.toml resource");
+        .setup(|_| {
             std::thread::spawn(move || {
-                start_mega(resource_path.to_str().unwrap());
+                start_mega();
             });
             Ok(())
         })
