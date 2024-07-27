8e16f5b07af02fe564b6b165a8a825211fcde5d2
benjamin.747 <benjamin.747@outlook.com>
2024-07-27 17:54:14 +0800

    add new routers for handle oauth requests

--- gateway/src/https_server.rs
+++ gateway/src/https_server.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::ops::Deref;
 use std::path::PathBuf;
@@ -14,10 +15,8 @@ use axum::routing::get;
 use axum::Router;
 use axum_server::tls_rustls::RustlsConfig;
 use clap::Args;
-use common::enums::ZtmType;
-use gemini::ztm::agent::{run_ztm_client, LocalZTMAgent};
-use gemini::ztm::hub::LocalZTMHub;
 use regex::Regex;
+use tokio::sync::Mutex;
 use tower::ServiceBuilder;
 use tower_http::cors::{Any, CorsLayer};
 use tower_http::decompression::RequestDecompressionLayer;
@@ -26,11 +25,15 @@ use tower_http::trace::TraceLayer;
 use ceres::lfs::LfsConfig;
 use ceres::protocol::{SmartProtocol, TransportProtocol};
 use common::config::Config;
+use common::enums::ZtmType;
 use common::model::{CommonOptions, GetParams};
+use gemini::ztm::agent::{run_ztm_client, LocalZTMAgent};
+use gemini::ztm::hub::LocalZTMHub;
 use jupiter::context::Context;
 use jupiter::raw_storage::local_storage::LocalStorage;
 
 use crate::api::api_router::{self};
+use crate::api::oauth::{self, OauthServiceState};
 use crate::api::ApiServiceState;
 use crate::ca_server::run_ca_server;
 use crate::lfs;
@@ -140,13 +143,25 @@ pub async fn app(config: Config, host: String, port: u16, common: CommonOptions)
         common: common.clone(),
     };
 
-    let api_state = ApiServiceState { context };
+    let api_state = ApiServiceState {
+        context: context.clone(),
+    };
 
     // add RequestDecompressionLayer for handle gzip encode
     // add TraceLayer for log record
     // add CorsLayer to add cors header
     Router::new()
-        .nest("/api/v1", api_router::routers().with_state(api_state))
+        .nest(
+            "/api/v1",
+            api_router::routers().with_state(api_state.clone()),
+        )
+        .nest(
+            "/auth",
+            oauth::routers().with_state(OauthServiceState {
+                context,
+                sessions: Arc::new(Mutex::new(HashMap::new())),
+            }),
+        )
         .route(
             "/*path",
             get(get_method_router)
