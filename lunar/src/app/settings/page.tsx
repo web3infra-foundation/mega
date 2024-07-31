2f5d3f22d493ab404254d1c29c47de5414b71672
benjamin.747 <benjamin.747@outlook.com>
2024-07-31 17:31:15 +0800

    update lunar ui, add settings page

---
+++ lunar/src/app/settings/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { Button } from '@/components/catalyst/button'
+import { Divider } from '@/components/catalyst/divider'
+import { Heading, Subheading } from '@/components/catalyst/heading'
+import { Input } from '@/components/catalyst/input'
+import { Text } from '@/components/catalyst/text'
+
+export default function Settings() {
+  return (
+    <form method="post" className="mx-auto max-w-4xl">
+      <Heading>Settings</Heading>
+      <Divider className="my-10 mt-6" />
+
+      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
+        <div className="space-y-1">
+          <Subheading>ZTM Server IP Address</Subheading>
+          <Text>This will be restart ZTM server.</Text>
+        </div>
+        <div>
+          <Input aria-label="Organization Name" name="name" defaultValue="http://34.84.172.121/relay" />
+        </div>
+      </section>
+
+      <Divider className="my-10" soft />
+
+      <div className="flex justify-end gap-4">
+        <Button type="reset" plain>
+          Reset
+        </Button>
+        <Button type="submit">Save changes</Button>
+      </div>
+    </form>
+  )
+}
