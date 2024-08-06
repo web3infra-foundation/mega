11b1302f0bfac3642975b6315446b958a5aa978a
Hou Xiaoxuan <Hou_Xiaoxuan@163.com>
2024-08-06 17:28:39 +0800

    feat: atlas add GiteeAI Serverless API client implementation
    Signed-off-by: Hou Xiaoxuan <Hou_Xiaoxuan@163.com>

--- atlas/src/api/mod.rs
+++ atlas/src/api/mod.rs
@@ -1,6 +1,7 @@
 pub mod gemini;
 pub mod lingyiwanwu;
 pub mod openai;
+pub mod gitee;
 #[cfg(test)]
 mod test {
     use std::env;
@@ -12,6 +13,11 @@ mod test {
 
     pub fn get_01_key() -> Option<String> {
         // Some("".to_string())
-        env::var("GOOGLE_GEMINI_KEY").unwrap().into()
+        env::var("LINGYI_KEY").unwrap().into()
+    }
+
+    pub fn get_giteeai_key() -> Option<String> {
+        // Some("".to_string())
+        env::var("GITEEAI_KEY").unwrap().into()
     }
 }
