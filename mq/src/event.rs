6e2dfa52b2ff70d9fc99af6ff30b46671059b0bf
Neon <yyk1249501542@gmail.com>
2024-08-06 09:55:12 +0800

    make mq a new module

---
+++ mq/src/event.rs
@@ -0,0 +1,18 @@
+use std::any::Any;
+
+pub(crate) type Message = Box<dyn EventBase>;
+
+pub trait EventBase: Send + Sync + Any + std::fmt::Display {
+    // async fn process(&self);
+}
+
+// impl std::fmt::Display for Event {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         match self {
+//             Event::Api(evt) => write!(f, "{}", evt),
+
+//             #[allow(unreachable_patterns)]
+//             _ => write!(f, "Unknown Event Type")
+//         }
+//     }
+// }
