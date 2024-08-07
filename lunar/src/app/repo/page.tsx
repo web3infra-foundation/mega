e847fe515ee767d8799463968f773eab594c927b
benjamin.747 <benjamin.747@outlook.com>
2024-08-07 17:52:41 +0800

    add start,stop command in tauri for settings page

--- lunar/src/app/repo/page.tsx
+++ lunar/src/app/repo/page.tsx
@@ -1,34 +1,23 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
 import DataList from '@/components/DataList'
+import { useRepoList } from '../api/fetcher';
+import { Skeleton } from "antd";
 
 
-export default function HomePage() {
-  const [repo_list, setRepoList] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let repo_list = await getRepoList();
-        setRepoList(repo_list);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
-  }, []);
+export default function RepoPage() {
+  const { data, isLoading, isError } = useRepoList();
 
   return (
     <div>
-      <DataList data={repo_list} />
+      {
+        isLoading &&
+        <Skeleton />
+      }
+      {
+        !isLoading &&
+        <DataList data={data} />
+      }
     </div>
   )
-}
-
-async function getRepoList() {
-  const res = await fetch(`api/relay/repo_list`);
-  const response = await res.json();
-  const repo_list = response.data;
-  return repo_list
 }
