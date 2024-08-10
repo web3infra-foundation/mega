27a4a177f84859d21aa330ae37e8078f77eb300e
Hou Xiaoxuan <Hou_Xiaoxuan@163.com>
2024-08-11 03:00:01 +0800

    feat: Add Claude API client implementation
    Signed-off-by: Hou Xiaoxuan <Hou_Xiaoxuan@163.com>

--- atlas/src/api/mod.rs
+++ atlas/src/api/mod.rs
@@ -1,7 +1,8 @@
+pub mod claude;
 pub mod gemini;
+pub mod gitee;
 pub mod lingyiwanwu;
 pub mod openai;
-pub mod gitee;
 #[cfg(test)]
 mod test {
     use std::env;
@@ -19,5 +20,10 @@ mod test {
     pub fn get_giteeai_key() -> Option<String> {
         // Some("".to_string())
         env::var("GITEEAI_KEY").unwrap().into()
+    }
+
+    pub fn get_claude_key() -> Option<String> {
+        // Some("".to_string())
+        env::var("CLAUDE_KEY").unwrap().into()
     }
 }
