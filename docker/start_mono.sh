e583973364e22fb318034c6729bc687c2d510326
HouXiaoxuan <Hou_Xiaoxuan@163.com>
2024-08-14 18:34:14 +0800

    chore: update Dockerfiles and start scripts for Mono services, add Dockerfiles for Moon srvices
    Signed-off-by: HouXiaoxuan <Hou_Xiaoxuan@163.com>

--- docker/start_mono.sh
+++ docker/start_mono.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 CONFIG_FILE="/etc/mega"
-MEGA_BASE_DIR = "/etc/mega"
+MEGA_BASE_DIR="/etc/mega"
 # check if config file exists
 if [ -f "$CONFIG_FILE" ]; then
     exec /opt/mega/target/debug/mono -c "$CONFIG_FILE"  service http --host 0.0.0.0
