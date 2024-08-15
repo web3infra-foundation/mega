c94b862db9b6939cc9f2b30f9fab7736919e4968
benjamin.747 <benjamin.747@outlook.com>
2024-08-15 16:12:01 +0800

    add repo publish api and tree, blob page

--- lunar/src/app/api/relay/repo_fork/route.ts
+++ lunar/src/app/api/relay/repo_fork/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
     const identifier = searchParams.get('identifier')
     const port = searchParams.get('port')
-    const res = await fetch(`${endpoint}/api/v1/ztm/repo_folk?identifier=${identifier}&port=${port}`, {
+    const res = await fetch(`${endpoint}/api/v1/mega/ztm/repo_fork?identifier=${identifier}&port=${port}`, {
     })
     const data = await res.json()
     
