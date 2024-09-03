00d9204bd5cdd8adcea405dfe79c6b924f61ea5a
benjamin.747 <benjamin.747@outlook.com>
2024-09-03 15:40:01 +0800

    [ui] update redirect with route handler

--- docker/start-moon.sh
+++ docker/start-moon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# user must set the MEGA_HOST,MEGA_INTERNAL_HOST and MOON_HOST
+# user must set the MEGA_HOST,MEGA_INTERNAL_HOST
 if [ -z "$MEGA_HOST" ]; then 
   echo "MEGA_HOST is not set"
   exit 1
@@ -8,11 +8,6 @@ fi
 
 if [ -z "$MEGA_INTERNAL_HOST" ]; then 
   echo "MEGA_INTERNAL_HOST is not set"
-  exit 1
-fi
-
-if [ -z "$MOON_HOST" ]; then 
-  echo "MOON_HOST is not set"
   exit 1
 fi
 
