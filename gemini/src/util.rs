580b3fd96368ed76081525ab7238a2b450ac6e69
wujian0327 <353981613@qq.com>
2024-10-09 10:53:05 +0800

    fix clippy

--- gemini/src/util.rs
+++ gemini/src/util.rs
@@ -70,8 +70,5 @@ pub async fn get_git_model_by_path(context: Context, path: String) -> Option<git
         .find_git_repo_exact_match(path.as_str())
         .await;
 
-    match git_model {
-        Ok(r) => r,
-        Err(_) => None,
-    }
+    git_model.unwrap_or_default()
 }
