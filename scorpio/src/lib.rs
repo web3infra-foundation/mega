421c59eb3769bbbf4eed247bc692458e0fc79ceb
Luxian <lux1an@qq.com>
2024-11-07 14:53:34 +0800

    [scorpio]: Overwrite the Megafuse module to async.
    Signed-off-by: Luxian <lux1an@qq.com>

--- scorpio/src/lib.rs
+++ scorpio/src/lib.rs
@@ -4,12 +4,12 @@
 extern crate log;
 mod passthrough;
 mod overlayfs;
-//mod store;
-// pub mod fuse;
+
+pub mod fuse;
 mod dicfuse;
 mod util;
-// pub mod manager;
+pub mod manager;
 pub mod server;
-// pub mod deamon;
+pub mod deamon;
 //const VFS_MAX_INO: u64 = 0xff_ffff_ffff_ffff;
 pub const READONLY_INODE :u64 = 0xffff_ffff;
