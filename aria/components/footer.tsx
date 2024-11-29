51ce2d272a59b962764bf81d63fdad7eda16446b
Eli Ma <eli@patch.sh>
2024-11-29 21:08:21 +0800

    Add mega favicon.ico and logo in footer
    Signed-off-by: Eli Ma <eli@patch.sh>

--- aria/components/footer.tsx
+++ aria/components/footer.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
-import { buttonVariants } from "./ui/button";
-import { CommandIcon, HeartIcon, TriangleIcon } from "lucide-react";
+import { HeartIcon, TriangleIcon } from "lucide-react";
+
+import { buttonVariants } from "@/components/ui/button";
+import { Logo } from "@/components/navbar";
 
 export function Footer() {
   return (
     <footer className="border-t w-full h-16">
       <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
         <div className="flex items-center gap-3">
-          <CommandIcon className="sm:block hidden w-5 h-5 text-muted-foreground" />
+          {/*<CommandIcon className="sm:block hidden w-5 h-5 text-muted-foreground" />*/}
+            <Logo />
           <p className="text-center">
             Build by{" "}
             <Link
