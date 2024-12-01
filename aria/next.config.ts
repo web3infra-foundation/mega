6fcc612fc4033104f0c164ddc4be2f59e283e92a
Quanyi Ma <eli@patch.sh>
2024-12-01 21:37:44 +0800

    Modify doc and blog of website
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- aria/next.config.ts
+++ aria/next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.freepik.com/**",
-      },
-    ],
-  },
-  // if used turbopack
-  // transpilePackages: ["next-mdx-remote"],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
