d3d5e1bbb639bc87b7278160fa73c069b0af4f58
Qihang Cai <mrbeanc@qq.com>
2024-12-05 15:47:00 +0800

    feat(orion): add webhook for notifying after building
    Signed-off-by: Qihang Cai <mrbeanc@qq.com>

--- orion/src/api.rs
+++ orion/src/api.rs
@@ -2,7 +2,7 @@ use axum::{Json, Router};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::post;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use crate::buck_controller;
 
 pub fn routers() -> Router {
@@ -14,19 +14,55 @@ struct BuildRequest {
     repo: String,
     target: String,
     args: Option<Vec<String>>,
+    webhook: Option<String>, // post
+}
+
+#[derive(Debug, Serialize)]
+struct BuildResponse {
+    success: bool,
+    message: String,
 }
 
 async fn buck_build(Json(request): Json<BuildRequest>) -> impl IntoResponse {
     let repo = request.repo;
     let target = request.target;
     let args = request.args.unwrap_or_default();
+    // TODO: async, notify by callback http api with build id
     tokio::task::spawn_blocking(move || {
-        match buck_controller::build(repo, target, args) {
+        let build_resp = match buck_controller::build(repo, target, args) {
             Ok(output) => {
                 tracing::info!("Build success: {}", output);
+                BuildResponse {
+                    success: true,
+                    message: output,
+                }
             }
             Err(e) => {
                 tracing::error!("Build failed: {}", e);
+                BuildResponse {
+                    success: false,
+                    message: e.to_string(),
+                }
+            }
+        };
+
+        // notify webhook
+        if let Some(webhook) = request.webhook {
+            let client = reqwest::blocking::Client::new();
+            let resp = client.post(webhook.clone())
+                .json(&build_resp)
+                .send();
+            match resp {
+                Ok(resp) => {
+                    if resp.status().is_success() {
+                        tracing::info!("Webhook notify success: {}", webhook);
+                    } else {
+                        tracing::error!("Webhook notify failed: {}", resp.status());
+                    }
+                }
+                Err(e) => {
+                    tracing::error!("Webhook notify failed: {}", e);
+                }
             }
         }
     });
