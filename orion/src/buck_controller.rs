a5ab6e2f8a62405ebde8f56d333c316ca74c8dca
Qihang Cai <mrbeanc@qq.com>
2024-12-06 22:20:37 +0800

    feat(orion): write build output to file
    Signed-off-by: Qihang Cai <mrbeanc@qq.com>

--- orion/src/buck_controller.rs
+++ orion/src/buck_controller.rs
@@ -1,23 +1,26 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 const PROJECT_ROOT: &str = "/home/bean/projects/buck2";
 
-pub fn build(repo: String, target: String, args: Vec<String>) -> Result<String, String> {
+pub fn build(repo: String, target: String, args: Vec<String>, id: String) -> std::io::Result<String> {
+    const BUILD_LOG_DIR: &str = "/tmp/buck2ctl";
+    std::fs::create_dir_all(BUILD_LOG_DIR)?;
+    let output_file = std::fs::File::create(&format!("{}/{}", BUILD_LOG_DIR, id))?;
+
     let mut cmd = Command::new("buck2");
     let cmd = cmd
         .arg("build")
         .args(args)
         .arg(target)
-        .current_dir(&format!("{}/{}", PROJECT_ROOT, repo));
+        .current_dir(&format!("{}/{}", PROJECT_ROOT, repo))
+        .stdout(Stdio::from(output_file.try_clone()?))
+        .stderr(Stdio::from(output_file));
+    // actually, some info (like: "BUILD SUCCESSFUL") is printed to stderr
 
     tracing::debug!("cmd:{:?}", cmd);
 
-    let output = cmd.output().map_err(|err| err.to_string())?;
-    if !output.status.success() {
-        return Err(String::from_utf8_lossy(&output.stderr).to_string());
-    }
+    let mut child = cmd.spawn()?;
+    child.wait()?;
 
-    // actually, some info (like: "BUILD SUCCESSFUL") is printed to stderr
-    let output = String::from_utf8_lossy(&output.stdout);
-    Ok(output.to_string())
+    Ok("BUILD SUCCEEDED".to_string())
 }
