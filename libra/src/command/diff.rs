web3infra-foundation/mega

commit 3ddc23d204c1cfc61248a8ee6fb7bdf59863ced0

[scorpio]: fix clippy for mega/gemini/libra  v1.83.0.
Signed-off-by: Han Xiaoyang <lux1an@qq.com>

libra/src/command/diff.rs
@@ -64,6 +64,9 @@ pub async fn execute(args: DiffArgs) {
         .stdin(Stdio::piped())
         .spawn()
         .expect("failed to execute process");
+        //.wait().unwrap();
+    #[cfg(unix)]
+    let _ = child.wait();
 
     let mut w = match args.output {
         Some(ref path) => {
@@ -138,13 +141,14 @@ pub async fn execute(args: DiffArgs) {
     match w {
         Some(ref mut file) => {
             file.write_all(&buf).unwrap();
+            
+           
         }
         None => {
             #[cfg(unix)]
             {
                 let stdin = child.stdin.as_mut().unwrap();
                 stdin.write_all(&buf).unwrap();
-                child.wait().unwrap();
             }
             #[cfg(not(unix))]
             {

gemini/src/http/handler.rs
@@ -80,7 +80,7 @@ pub fn get_peer_id_from_identifier(identifier: String) -> Result<String, String>
     if words.len() <= 2 {
         return Err("invalid identifier".to_string());
     }
-    return Ok(words.get(2).unwrap().to_string());
+    Ok(words.get(2).unwrap().to_string())
 }
 
 pub fn get_alias_from_identifier(identifier: String) -> Result<String, String> {
@@ -89,7 +89,7 @@ pub fn get_alias_from_identifier(identifier: String) -> Result<String, String> {
     if words.len() <= 3 {
         return Err("invalid identifier".to_string());
     }
-    return Ok(words.get(3).unwrap().to_string());
+    Ok(words.get(3).unwrap().to_string())
 }
 
 pub async fn get_git_path_by_alias(

gemini/src/lfs/mod.rs
@@ -175,7 +175,7 @@ pub fn get_file_hash_from_origin(origin: String) -> Result<String, String> {
     if words.len() <= 4 {
         return Err("invalid origin".to_string());
     }
-    return Ok(words.get(4).unwrap().to_string());
+    Ok(words.get(4).unwrap().to_string())
 }
 
 #[cfg(test)]

mega/tests/lfs_test.rs
@@ -229,6 +229,7 @@ fn lfs_split_with_git() {
     git_lfs_clone(url).expect("Failed to clone large file from mega server");
 
     mega.kill().expect("Failed to kill mega server");
+    let _ = mega.wait();
     thread::sleep(Duration::from_secs(1)); // wait for server to stop, avoiding affecting other tests
 }
 
@@ -253,5 +254,6 @@ fn lfs_split_with_libra() {
 
     env::set_var("MEGA_authentication__enable_http_auth", "false"); // avoid affecting other tests
     mega.kill().expect("Failed to kill mega server");
+    let _ = mega.wait();
     thread::sleep(Duration::from_secs(1)); // wait for server to stop, avoiding affecting other tests
 }

commit 3d2ef21f0bc31d1232c46d570ee625f1b91b8ad3

fix(libra): resolve errors caused by conflict handling in 3ddc23d2
Signed-off-by: HouXiaoxuan <Hou_Xiaoxuan@163.com>

libra/src/command/diff.rs
@@ -64,9 +64,6 @@ pub async fn execute(args: DiffArgs) {
         .stdin(Stdio::piped())
         .spawn()
         .expect("failed to execute process");
-        //.wait().unwrap();
-    #[cfg(unix)]
-    let _ = child.wait();
 
     let mut w = match args.output {
         Some(ref path) => {
@@ -135,14 +132,13 @@ pub async fn execute(args: DiffArgs) {
     match w {
         Some(ref mut file) => {
             file.write_all(&buf).unwrap();
-            
-           
         }
         None => {
             #[cfg(unix)]
             {
                 let stdin = child.stdin.as_mut().unwrap();
                 stdin.write_all(&buf).unwrap();
+                let _ = child.wait();
             }
             #[cfg(not(unix))]
             {