5b1b3b9dd8d77223855674df352857af99394eea
besscroft <besscroft@foxmail.com>
2024-12-12 16:53:19 +0800

    ci: update next.config.js

--- moon/next.config.js
+++ moon/next.config.js
@@ -2,6 +2,9 @@
 
 const nextConfig = {
     output: "standalone",
+    compiler: {
+        removeConsole: process.env.NODE_ENV === "production",
+    },
     images: {
         remotePatterns: [
             {
