0552dfab1739eea1f2e46090dd67925da55ed533
Qihang Cai <mrbeanc@qq.com>
2024-12-16 21:55:44 +0800

    feat(orion): add database support for build outputs
    backup currently
    more async
    
    Signed-off-by: Qihang Cai <mrbeanc@qq.com>

--- orion/src/buck_controller.rs
+++ orion/src/buck_controller.rs
@@ -1,10 +1,11 @@
 use std::io;
-use std::process::{Command, Stdio};
+use std::process::ExitStatus;
+use tokio::process::Command;
 use crate::util;
 
 const PROJECT_ROOT: &str = "/home/bean/projects/buck2";
 
-pub fn build(repo: String, target: String, args: Vec<String>, log_path: String) -> io::Result<String> {
+pub async fn build(repo: String, target: String, args: Vec<String>, log_path: String) -> io::Result<ExitStatus> {
     util::ensure_parent_dirs(&log_path)?;
     let output_file = std::fs::File::create(log_path)?;
 
@@ -14,14 +15,14 @@ pub fn build(repo: String, target: String, args: Vec<String>, log_path: String) 
         .args(args)
         .arg(target)
         .current_dir(&format!("{}/{}", PROJECT_ROOT, repo))
-        .stdout(Stdio::from(output_file.try_clone()?))
-        .stderr(Stdio::from(output_file));
+        .stdout(output_file.try_clone()?)
+        .stderr(output_file);
     // actually, some info (like: "BUILD SUCCESSFUL") is printed to stderr
 
     tracing::debug!("cmd:{:?}", cmd);
 
     let mut child = cmd.spawn()?;
-    child.wait()?;
+    let status = child.wait().await?;
 
-    Ok("BUILD ENDED".to_string())
+    Ok(status)
 }
