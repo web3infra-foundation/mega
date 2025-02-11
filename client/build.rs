f965b17f1d2995825924158bca9cf47d095ee856
Neon <yyk1249501542@gmail.com>
2025-02-11 10:15:59 +0800

    manually compile on windows

--- client/build.rs
+++ client/build.rs
@@ -5,12 +5,14 @@ fn main() {
     // https://gtk-rs.org/gtk4-rs/stable/latest/book/settings.html
     let schemar_dir = {
         #[cfg(target_os = "windows")]
-        "C:/ProgramData/glib-2.0/schemas/";
+        {
+            "C:/ProgramData/glib-2.0/schemas/".to_string()
+        }
         #[cfg(not(target_os = "windows"))]
         {
             let mut dir = std::env::var("HOME").expect("Failed to get HOME");
             dir.push_str("/.local/share/glib-2.0/schemas");
-            dir
+            dir.to_string()
         }
     }
     .parse::<PathBuf>()
@@ -26,7 +28,6 @@ fn main() {
         .arg(schemar_dir.to_str().unwrap())
         .output()
         .expect("Failed to compile schemas, did you install the package `glibc2`?");
-
 
     // Step.2 Compile the resources
     let current_dir = std::env!("CARGO_MANIFEST_DIR")
