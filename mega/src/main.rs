b9890f14580966c34c66137f9e2c06fa27db30b1
hanbings <hanbings@hanbings.io>
2025-02-13 14:44:51 +0800

    Change global allocator for mega and mono.
    Signed-off-by: hanbings <hanbings@hanbings.io>

--- mega/src/main.rs
+++ mega/src/main.rs
@@ -7,6 +7,14 @@ shadow!(build);
 mod cli;
 mod commands;
 
+#[cfg(not(target_os = "windows"))]
+#[global_allocator]
+static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+#[cfg(target_os = "windows")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 fn main() {
     // Parse the command line arguments
     let result = cli::parse(None);
