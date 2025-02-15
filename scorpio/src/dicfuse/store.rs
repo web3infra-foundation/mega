89e84a6c4e83ee20ea8b5009f7a966bc568575b7
Han Xiaoyang <lux1an@qq.com>
2025-02-15 19:08:47 +0800

    [scorpio]: Add FUSE interface support for persistent directory storage.
    Signed-off-by: Han Xiaoyang <lux1an@qq.com>

--- scorpio/src/dicfuse/store.rs
+++ scorpio/src/dicfuse/store.rs
@@ -8,19 +8,17 @@ use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use tokio::sync::Mutex;
 
+use core::panic;
 use std::io;
-
-
 use std::{collections::HashMap, error::Error};
 use std::collections::VecDeque;
 use once_cell::sync::Lazy;
-use radix_trie::{self, TrieCommon};
 use std::sync::Arc;
 use crate::util::atomic::AtomicU64;
 use crate::READONLY_INODE;
 
 use super::abi::{default_dic_entry, default_file_entry};
-use super::tree_store::TreeStorage;
+use super::tree_store::{StorageItem, TreeStorage};
 use crate::util::GPath;
 const MEGA_TREE_URL: &str = "localhost:8000";//TODO: make it configable
 const UNKNOW_INODE: u64 = 0; // illegal inode number;
@@ -33,7 +31,11 @@ pub struct Item {
     pub path: String,
     pub content_type: String,
 }
-
+impl Item {
+    pub fn is_dir(&self)->bool{
+        self.content_type == INODE_DICTIONARY
+    }
+}
 #[allow(unused)]
 pub struct DicItem{
     inode:u64,
@@ -136,60 +138,13 @@ pub struct DictionaryStore {
     
     inodes: Arc<Mutex<HashMap<u64, Arc<DicItem>>>>,
     next_inode: AtomicU64,
-    queue: Arc<Mutex<VecDeque<u64>>>,
     radix_trie: Arc<Mutex<radix_trie::Trie<String, u64>>>,
     persistent_path_store :Arc<Mutex<TreeStorage>>,// persistent path store for saving and retrieving file paths
 }
 
 #[allow(unused)]
 impl DictionaryStore {
-    #[allow(clippy::await_holding_lock)]
-    pub async fn async_import(&self){
     
-            let items = fetch_tree("").await.unwrap().data.clone() ;
-
-            let root_inode: Arc<DicItem> = self.inodes.lock().await.get(&1).unwrap().clone();
-            for it in items{
-                println!("root item:{:?}",it);
-                self.update_inode(root_inode.clone(),it).await;
-            }
-            loop {//BFS to look up all dictionary
-                if self.queue.lock().await.is_empty(){
-                    break;
-                }
-                let one_inode = self.queue.lock().await.pop_front().unwrap();
-                let mut new_items = Vec::new();
-                {
-                    let it = self.inodes.lock().await.get(&one_inode).unwrap().clone();
-                    let mut ct =it.content_type.lock().await;
-                    let path=String::new();
-                    if let ContentType::Directory(load) = *ct{
-                        if !load{
-                            *ct = ContentType::Directory(true);
-                            let path = it.get_path();
-                            println!("fetch path :{}",path);
-                        }
-                        
-                        if path.len()>1{
-                            drop(ct);
-                            let t = fetch_tree(&path.clone()).await;
-                            new_items = t.unwrap().data.clone() ;
-                        }
-                    }
-
-                   
-                    let mut pc = it.clone();
-                    for newit in new_items {
-                        println!("import item :{:?}",newit);
-                        self.update_inode(pc.clone(),newit).await; // Await the update_inode call
-                    }
-                    
-                }
-                new_items = Vec::new();
-            }
-            //queue.clear();
-        
-    }
     pub async fn new() -> Self {
         let tree_store =  TreeStorage::new().expect("Failed to create TreeStorage");
         tree_store.insert_item(1, UNKNOW_INODE, Item{
@@ -201,7 +156,6 @@ impl DictionaryStore {
             next_inode: AtomicU64::new(2),
             inodes: Arc::new(Mutex::new(HashMap::new())),
             radix_trie: Arc::new(Mutex::new(radix_trie::Trie::new())),
-            queue: Arc::new(Mutex::new(VecDeque::new())),
             persistent_path_store:  Arc::new(Mutex::new(tree_store))
         };
         let root_item = DicItem{
@@ -215,112 +169,121 @@ impl DictionaryStore {
         init.inodes.lock().await.insert(1, root_item.into());
         init
     }
-    async fn  update_inode(&self,pitem:Arc<DicItem>,item:Item){
+    async fn update_inode(&self,parent: u64,item:Item) ->std::io::Result<u64> {
         self.next_inode.fetch_add(1).await;
         
         let alloc_inode = self.next_inode.load().await;
         
         assert!(alloc_inode < READONLY_INODE );
-        if item.content_type==INODE_DICTIONARY{
-            self.queue.lock().await.push_back(alloc_inode);
+        
+        let prw = self.persistent_path_store.lock().await;
+        if let Ok(pinode) = prw.get_item(parent){
+            // insert info to a radix_trie for path match.
+            self.radix_trie.lock().await.insert(GPath::from(item.path.clone()).to_string(), alloc_inode);
+            prw.insert_item(alloc_inode, parent, item);
+            //prw.append_child(parent, alloc_inode);
+        }else{
+            //error...
+            return Err(io::Error::new(io::ErrorKind::NotFound, "Parent inode not found"));
         }
-        
-        
-        let parent = pitem;
-        let newitem = Arc::new(DicItem::new(alloc_inode, parent.get_inode(),item.clone()));
-       
-        parent.push_children(newitem.clone()).await;
-        self.radix_trie.lock().await.insert(newitem.get_path(), alloc_inode);
-        self.inodes.lock().await.insert(alloc_inode, newitem);
-        self.persistent_path_store.lock().await.insert_item(alloc_inode, parent.get_inode(), item);
 
+        Ok(alloc_inode)
     }
 
     pub async fn import(&self){
-        // 在阻塞线程中运行异步任务
+        
         let items =  fetch_tree("").await.unwrap().data;
         
         let root_inode = self.inodes.lock().await.get(&1).unwrap().clone();
-
-
+        // deque for bus.
+        let mut queue= VecDeque::<u64>::new(); 
         for it in items{
-            println!("root item:{:?}",it);
-            self.update_inode(root_inode.clone(),it).await;
+            let is_dir = it.content_type ==INODE_DICTIONARY;
+            let it_inode = self.update_inode(1,it).await.unwrap();
+            if is_dir{
+                queue.push_back(it_inode);
+            }
         }
+        
         loop {//BFS to look up all dictionary
-            if self.queue.lock().await.is_empty(){
+            if queue.is_empty(){
                 break;
             }
-            let one_inode = self.queue.lock().await.pop_front().unwrap();
+            let one_inode = queue.pop_front().unwrap();
             let mut new_items = Vec::new();
-            {
-                let it = self.inodes.lock().await.get(&one_inode).unwrap().clone();
-                if let ContentType::Directory(load) = *it.content_type.lock().await{
-                    if !load{
-                        let path = it.get_path();
-                        println!("fetch path :{}",path);
-                        
-                        // 在阻塞线程中运行异步任务
-                        new_items =fetch_tree(&path).await.unwrap().data;
-                
-                    }
-                   
+            
+            let it = self.persistent_path_store.lock().await.get_all_path(one_inode).unwrap();
+            let path = it.to_string();
+            println!("fetch path :{}",path);
+            // get tree by parent inode.
+            new_items =fetch_tree(&path).await.unwrap().data;
+            
+            // Insert all new inode.
+            for newit in new_items {
+                println!("import item :{:?}",newit);
+                let is_dir = newit.is_dir();
+                let new_inode = self.update_inode(one_inode,newit).await.unwrap(); // Await the update_inode call
+                // push to queue to BFS.
+                if is_dir{
+                    queue.push_back(new_inode);
                 }
-                let mut pc = it.clone();
-                for newit in new_items {
-                    println!("import item :{:?}",newit);
-                    self.update_inode(pc.clone(),newit).await; // Await the update_inode call
-                }
-                let mut content_type = pc.content_type.lock().await;
-                *content_type = ContentType::Directory(true);
             }
-            new_items = Vec::new();
+        
         }
         //queue.clear();
     }
     
     pub async fn find_path(&self,inode :u64)-> Option<GPath>{
-        self.inodes.lock().await.get(&inode).map(|item| item.path_name.clone())
-    }
-    pub async fn get_inode(&self,inode: u64) -> Result<Arc<DicItem>, io::Error> {
-        match self.inodes.lock().await.get(&inode) {
-            Some(item) => Ok(item.clone()),
-            None=>Err(io::Error::new(io::ErrorKind::NotFound, "inode not found"))
+
+        match self.persistent_path_store.lock().await.get_all_path(inode) {
+            Ok(a) => Some(a),
+            Err(_) => None,
         }
-       
+
     }
-    pub async fn get_by_path(&self, path: &str) -> Result<Arc<DicItem>, io::Error> {
-        let binding = self.radix_trie.lock().await;
-        let inode = binding.get(path).ok_or(io::Error::new(io::ErrorKind::NotFound, "path not found"))?;
-        self.get_inode(*inode).await
+    pub async fn get_inode(&self,inode: u64) -> Result<StorageItem, io::Error> {
+        self.persistent_path_store.lock().await.get_item(inode)
     }
-    async fn find_children(&self,parent: u64) -> Result<DicItem,io::Error>{
-        let path = self.inodes.lock().await.get(&parent).map(|item| item.path_name.clone());
-        if let Some(parent_path) = path{
-            let l  = self.radix_trie.lock().await;
-            let pathstr:String =parent_path.name();
-            let u = l.subtrie(&pathstr).unwrap();
-            let c = u.children();
-        }
-        todo!()
+    
+    pub async fn get_by_path(&self, path: &str) -> Result<StorageItem, io::Error> {
+        let inode = 
+        if path.is_empty() || path=="/"{
+             1
+        }else{
+            let binding = self.radix_trie.lock().await;
+            *binding
+                .get(path)
+                .ok_or(io::Error::new(io::ErrorKind::NotFound, "path not found"))?
+               
+        };
+        
+        self.get_inode(inode).await
     }
-    pub async fn do_readdir(&self,parent:u64,fh:u64,offset:u64) -> Result<Vec<Arc<DicItem>>, io::Error>{
+
+    pub async fn do_readdir(&self,parent:u64,fh:u64,offset:u64) -> Result<Vec<StorageItem>, io::Error>{
         //  1. 获取目录项
-        let dictionary = self.get_inode(parent).await?;
-        let p_dictionary = self.get_inode(dictionary.get_inode()).await?;
-        let mut re = vec![dictionary.clone(),p_dictionary.clone()];
+        let  item = self.get_inode(parent).await?; // current_dictionary
+        let  mut parent_path = self.find_path(parent).await.unwrap();
+        parent_path.pop();
+
+        let parent_item =self.get_by_path(&parent_path.to_string()).await?;
+
+        
+        let mut re = vec![item.clone(),parent_item.clone()];
         //let mut re = vec![];
          // 2. 确保目录项是一个目录
-         if let ContentType::Directory(_) = dictionary.get_tyep().await {
+         if item.is_dir(){
              // 3. 获取子目录项
-             let  children = dictionary.children.lock().await;
+             
+             let children = self.persistent_path_store.lock().await.get_children(parent)?;
              let mut total_bytes_written = 0;
              let mut current_offset = 0;
 
              // 4. 遍历子目录项
-             for (i, (name, child)) in children.iter().enumerate() {
+             for (i,  child) in children.iter().enumerate() {
                 re.push(child.clone());
              }
+             print!("readdri len :{}",re.len());
              Ok(re)
          } else {
              Err(io::Error::new(io::ErrorKind::NotFound, "Not a directory"))
@@ -329,6 +292,8 @@ impl DictionaryStore {
 }
 #[cfg(test)]
 mod tests {
+    use radix_trie::TrieCommon;
+
     use super::*;
     #[tokio::test]
     #[ignore] // This will prevent the test from running by default
