3742e6edfc3cb844549dc7719ef404b35b5a39e2
Neon <yyk1249501542@gmail.com>
2025-02-27 14:25:53 +0800

    fix ci/cd install-dep

--- jupiter/src/storage/init.rs
+++ jupiter/src/storage/init.rs
@@ -1,5 +1,5 @@
 use std::{path::Path, time::Duration};
-use std::error::Error;
+use common::errors::MegaError;
 use sea_orm::{ConnectOptions, ConnectionTrait, Database, DatabaseConnection, DbErr, Statement, TransactionError, TransactionTrait};
 use tracing::log;
 
@@ -26,7 +26,7 @@ pub async fn database_connection(db_config: &DbConfig) -> DatabaseConnection {
     }
 }
 
-async fn postgres_connection(db_config: &DbConfig) -> Result<DatabaseConnection, Box<dyn Error>> {
+async fn postgres_connection(db_config: &DbConfig) -> Result<DatabaseConnection, MegaError> {
     let db_url = db_config.db_url.to_owned();
     log::info!("Connecting to database: {}", db_url);
 
@@ -34,7 +34,7 @@ async fn postgres_connection(db_config: &DbConfig) -> Result<DatabaseConnection,
     Database::connect(opt).await.map_err(|e| e.into())
 }
 
-async fn sqlite_connection(db_config: &DbConfig) -> Result<DatabaseConnection, Box<dyn Error>> {
+async fn sqlite_connection(db_config: &DbConfig) -> Result<DatabaseConnection, MegaError> {
     if !Path::new(&db_config.db_path).exists() {
         eprintln!("Creating new sqlite database: {}", db_config.db_path);
         std::fs::create_dir_all(Path::new(&db_config.db_path).parent().unwrap())?;
@@ -45,11 +45,11 @@ async fn sqlite_connection(db_config: &DbConfig) -> Result<DatabaseConnection, B
 
     let opt = setup_option(db_url);
     let conn = Database::connect(opt).await?;
-    
+
     // setup sqlite database (execute .sql)
     if is_file_empty(&db_config.db_path) {
         log::info!("Setting up sqlite database");
-        setup_sql(&conn).await?;
+        setup_sql(&conn).await.map_err(|e| sea_orm::DbErr::Custom(e.to_string()))?;
     }
     Ok(conn)
 }
