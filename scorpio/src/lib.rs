8d49172fd927d49d887120f2da75918eed9ae6e1
Han Xiaoyang <lux1an@qq.com>
2025-03-21 22:32:37 +0800

    [scorpio]: LFS track & untrack
    Signed-off-by: Han Xiaoyang <lux1an@qq.com>

--- scorpio/src/lib.rs
+++ scorpio/src/lib.rs
@@ -9,5 +9,6 @@ pub mod util;
 pub mod manager;
 pub mod server;
 pub mod daemon;
+mod scolfs;
 //const VFS_MAX_INO: u64 = 0xff_ffff_ffff_ffff;
 const READONLY_INODE :u64 = 0xffff_ffff;
--- scorpio/src/scolfs/mod.rs
+++ scorpio/src/scolfs/mod.rs
@@ -1,6 +1,5 @@
-mod controller;
+//mod controller;
 mod lfs;
 mod utils;
-mod reg;
-use libra::utils::lfs as lfs_util;
+pub mod route;
 //use super::util;
