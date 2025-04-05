3b3a0c18b98c0508975da5f2eeecaa345879f24b
Neon <neonkk@qq.com>
2025-04-05 10:14:11 +0800

    fix: open file from tree

--- monobean/src/components/code_page.rs
+++ monobean/src/components/code_page.rs
@@ -4,7 +4,7 @@ use async_channel::Sender;
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::{glib, CompositeTemplate};
-use scv::{prelude::*, Buffer};
+use scv::{prelude::*, Buffer, StyleSchemeChooser};
 
 use crate::application::Action;
 
@@ -23,13 +23,11 @@ mod imp {
         // #[template_child]
         // pub search_entry: TemplateChild<gtk::SearchEntry>,
         #[template_child]
-        pub list_view: TemplateChild<gtk::ListView>,
-        #[template_child]
         pub file_tree_view: TemplateChild<FileTreeView>,
         #[template_child]
         pub code_stack: TemplateChild<gtk::Stack>,
         #[template_child]
-        pub empty_page: TemplateChild<gtk::Box>,
+        pub empty_view: TemplateChild<gtk::Box>,
         #[template_child]
         pub source_view: TemplateChild<scv::View>,
 
@@ -92,39 +90,71 @@ impl CodePage {
         let imp = self.imp();
 
         let file_tree_view = self.imp().file_tree_view.get();
-        file_tree_view.setup_file_tree(imp.sender.get().unwrap().clone(), PathBuf::from("E:/Projects/mega/"));
+        file_tree_view.setup_file_tree(
+            imp.sender.get().unwrap().clone(),
+            PathBuf::from("E:/Projects/mega/"),
+        );
     }
 
     fn setup_source_view(&self, opened_file: Option<&Path>) {
-        let buf = Buffer::new(None);
-        buf.set_highlight_syntax(true);
-        if let Some(ref language) = scv::LanguageManager::new().language("rust") {
-            buf.set_language(Some(language));
-        }
-        // if let Some(ref scheme) = scv::StyleSchemeManager::new().scheme("solarized-dark") {
-        //     buf.set_style_scheme(Some(scheme));
-        // }
+        let imp = self.imp();
+        let source_view = imp.source_view.get();
+        source_view.set_accepts_tab(true);
 
-        // FIXME: be care with os path
-        let pb = std::path::PathBuf::from("E:/Projects/mega/monobean/src/components/code_page.rs");
-        let file = adw::gio::File::for_path(&pb);
+        match opened_file {
+            Some(path) => {
+                self.show_editor(path);
+            }
+            None => {
+                self.hide_editor();
+            }
+        }
+    }
+
+    pub fn show_editor(&self, path: impl AsRef<Path>) {
+        let imp = self.imp();
+        let path = path.as_ref();
+        if !path.exists() || !path.is_file() {
+            tracing::warn!("file not exists: {:?}", path);
+            return;
+        }
+
+        let buf = Buffer::new(None);
+        let file_name = path.file_name().unwrap().to_str();
+        let language_manager = scv::LanguageManager::new();
+        let file = adw::gio::File::for_path(path);
         let file = scv::File::builder().location(&file).build();
         let loader = scv::FileLoader::new(&buf, &file);
+
+        buf.set_highlight_syntax(true);
+        if let Some(ref language) = language_manager.guess_language(file_name, None) {
+            tracing::debug!("Guessed language: {:?}", language);
+            buf.set_language(Some(language));
+        }
+        if let Some(ref scheme) = scv::StyleSchemeManager::new().scheme("Adwaita-dark") {
+            buf.set_style_scheme(Some(scheme));
+        }
+
         loader.load_async_with_callback(
             glib::Priority::default(),
             adw::gio::Cancellable::NONE,
             move |current_num_bytes, total_num_bytes| {
-                println!(
+                tracing::debug!(
                     "loading: {:?}",
-                    (current_num_bytes as f32 / total_num_bytes as f32) * 100f32
+                    (current_num_bytes as f32 / total_num_bytes as f32) * 100f32,
                 );
             },
             |res| {
-                println!("loaded: {:?}", res);
+                tracing::debug!("loaded: {}", res.is_ok());
             },
         );
 
-        self.imp().source_view.set_buffer(Some(&buf));
-        self.imp().code_stack.set_visible_child_name("source_view");
+        imp.source_view.set_buffer(Some(&buf));
+        imp.code_stack.set_visible_child_name("source_view");
+    }
+
+    pub fn hide_editor(&self) {
+        let imp = self.imp();
+        imp.code_stack.set_visible_child_name("empty_view");
     }
 }
