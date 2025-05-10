32f443ba16b78bd9a6a963f5c2d59ab9473a692c
benjamin.747 <benjamin.747@outlook.com>
2025-05-10 13:17:06 +0800

    fix(action): update UI action

--- docker/start-mono.sh
+++ docker/start-mono.sh
@@ -6,7 +6,7 @@ CONFIG_FILE="$MEGA_BASE_DIR/etc/config.toml"
 # check if config file exists
 if [ -f "$CONFIG_FILE" ]; then
     echo "Using config file: $CONFIG_FILE"
-    exec /usr/local/bin/mono -c "$CONFIG_FILE"  service multi http ssh --host 0.0.0.0 --http-port 8000 --ssh-port 9000
+    exec /usr/local/bin/mono -c "$CONFIG_FILE"  service multi http ssh --host 0.0.0.0 --port 8000 --ssh-port 9000
 else
     exec /usr/local/bin/mono service multi http ssh --host 0.0.0.0 --ssh-port 22
 fi
