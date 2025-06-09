2f70b33fc493921ecf61126fb6f96b18ee6d4e48
Eli Ma <eli@patch.sh>
2025-06-09 23:29:31 +0800

    Add a new service_test.rs for integration service with mono
    Signed-off-by: Eli Ma <eli@patch.sh>

---
+++ tests/service_test.rs
@@ -0,0 +1,17 @@
+
+#[cfg(test)]
+mod tests {
+    use reqwest;
+    use tokio;
+
+    #[tokio::test]
+    async fn check_mono_service_status() -> Result<(), reqwest::Error> {
+        let client = reqwest::Client::new();
+        let response = client.get("http://127.0.0.1:8000/api/v1/status")
+            .send()
+            .await?;
+        
+        assert_eq!(response.status(), reqwest::StatusCode::OK, "Service status API did not return 200 OK");
+        Ok(())
+    }
+}
