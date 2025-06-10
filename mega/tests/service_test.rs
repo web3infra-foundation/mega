0e8d029dfb2fc368a8698e2321588212c0bf2b32
Eli Ma <eli@patch.sh>
2025-06-10 16:37:01 +0800

    Fix the --skip service_test argument
    Signed-off-by: Eli Ma <eli@patch.sh>

--- mega/tests/service_test.rs
+++ mega/tests/service_test.rs
@@ -1,7 +1,9 @@
 use reqwest;
 use tokio;
+use serial_test::serial;
 
 #[tokio::test]
+#[serial]
 async fn check_mono_service_status() -> Result<(), reqwest::Error> {
     let client = reqwest::Client::new();
     let response = client.get("http://127.0.0.1:8000/api/v1/status")
