807286532053e41c2066fa000a892a869025d681
benjamin.747 <benjamin.747@outlook.com>
2025-06-20 17:13:57 +0800

    feat(UI): add label apis

---
+++ mono/src/api/label/mod.rs
@@ -0,0 +1,31 @@
+pub mod label_router;
+
+use callisto::label;
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+
+#[derive(Serialize, Deserialize, ToSchema)]
+pub struct LabelItem {
+    pub id: i64,
+    pub name: String,
+    pub color: String,
+    pub description: String,
+}
+
+impl From<label::Model> for LabelItem {
+    fn from(value: label::Model) -> Self {
+        Self {
+            id: value.id,
+            name: value.name,
+            color: value.color,
+            description: value.description,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, ToSchema)]
+pub struct NewLabel {
+    pub name: String,
+    pub color: String,
+    pub description: String,
+}
