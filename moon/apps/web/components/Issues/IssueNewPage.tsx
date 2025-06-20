bf2885a0bd373ccdf5d4d72ef9e913e55dd66801
sailong <sailong.rust@isrc.iscas.ac.cn>
2025-06-20 16:06:39 +0800

    refactor(UI): The issue page editing box uses the tiptap component

--- moon/apps/web/components/Issues/IssueNewPage.tsx
+++ moon/apps/web/components/Issues/IssueNewPage.tsx
@@ -1,23 +1,26 @@
 'use client'
 
-import { useCallback, useState } from 'react'
+import { useCallback, useState, useRef } from 'react'
 import { Flex, Input, Space } from 'antd/lib'
 import { useRouter } from 'next/router'
 import toast from 'react-hot-toast'
 
-import { Button, LargeTitle } from '@gitmono/ui'
-
-import RichEditor from '@/components/MrView/rich-editor/RichEditor'
+import { Button, LargeTitle, PicturePlusIcon } from '@gitmono/ui'
 import { usePostIssueSubmit } from '@/hooks/issues/usePostIssueSubmit'
 import { apiErrorToast } from '@/utils/apiErrorToast'
+import { SimpleNoteContent, SimpleNoteContentRef } from '@/components/SimpleNoteEditor/SimpleNoteContent';
+import { EMPTY_HTML } from '@/atoms/markdown'
+import { useHandleBottomScrollOffset } from '@/components/NoteEditor/useHandleBottomScrollOffset'
+import { useUploadHelpers } from '@/hooks/useUploadHelpers';
+import { ComposerReactionPicker } from '@/components/Reactions/ComposerReactionPicker';
+import { trimHtml } from '@/utils/trimHtml'
 
 export default function IssueNewPage() {
-  const [editorState, setEditorState] = useState('')
   const [title, setTitle] = useState('')
   const [loadings, setLoadings] = useState<boolean[]>([])
   const router = useRouter()
   const { mutate: submitNewIssue } = usePostIssueSubmit()
-  const [editorHasText, setEditorHasText] = useState(false)
+  const [isReactionPickerOpen, setIsReactionPickerOpen] = useState(false)
   const set_to_loading = (index: number) => {
     setLoadings((prevLoadings) => {
       const newLoadings = [...prevLoadings]
@@ -37,17 +40,19 @@ export default function IssueNewPage() {
   }
 
   const submit = useCallback(
-    (description: string) => {
-      if (JSON.parse(description).root.children[0].children.length === 0 || !title) {
+    () => {
+      const currentContentHTML = editorRef.current?.editor?.getHTML() ?? '<p></p>';
+      
+      if (trimHtml(currentContentHTML) === '' || !title) {
         toast.error('please fill the issue list first!')
         return
       }
       set_to_loading(3)
       submitNewIssue(
-        { data: { title, description } },
+        { data: { title, description: currentContentHTML } },
         {
           onSuccess: (_response) => {
-            setEditorState('')
+            editorRef.current?.clearAndBlur()
             cancel_loading(3)
             toast.success('success')
             router.push(`/${router.query.org}/issue`)
@@ -58,6 +63,14 @@ export default function IssueNewPage() {
     },
     [router, title, submitNewIssue]
   )
+
+  const editorRef = useRef<SimpleNoteContentRef>(null);
+  const onKeyDownScrollHandler = useHandleBottomScrollOffset({
+    editor: editorRef.current?.editor
+  })
+  const { dropzone } = useUploadHelpers({
+    upload: editorRef.current?.uploadAndAppendAttachments
+  })
 
   return (
     <>
@@ -76,10 +89,34 @@ export default function IssueNewPage() {
           </h1>
         </Space>
         <Space direction='vertical' style={{ width: '100%' }}>
-          <h1>Add a description</h1>
-          <RichEditor setEditorState={setEditorState} setEditorHasText={setEditorHasText} />
+        <div className="prose flex flex-col w-full mt-2">
+          <h2>Add a comment</h2>
+          <input {...dropzone.getInputProps()} />
+          <div className='border p-6 rounded-lg'>
+            <SimpleNoteContent
+              commentId="temp" //  Temporary filling, replacement later
+              ref={editorRef}
+              editable="all"
+              content={EMPTY_HTML}
+              autofocus={true}
+              onKeyDown={onKeyDownScrollHandler}
+            />
+            <Button
+              variant='plain'
+              iconOnly={<PicturePlusIcon />}
+              accessibilityLabel='Add files'
+              onClick={dropzone.open}
+              tooltip='Add files'
+            />
+            <ComposerReactionPicker
+              editorRef={editorRef}
+              open={isReactionPickerOpen}
+              onOpenChange={setIsReactionPickerOpen}
+            />
+          </div>
+        </div>
           <Flex justify={'flex-end'}>
-            <Button type={'submit'} disabled={!editorHasText} loading={loadings[3]} onClick={() => submit(editorState)}>
+            <Button type={'submit'} loading={loadings[3]} onClick={() => submit()}>
               Submit New Issue
             </Button>
           </Flex>
