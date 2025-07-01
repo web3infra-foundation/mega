0a8f8e4860a02f179c9fa6d8da6bacfd61480d36
Quanyi Ma <eli@patch.sh>
2025-07-01 19:53:22 +0800

    Remove archived from repo
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- archived/craft/src/lfs/commands/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod command_track;
-pub mod command_untrack;
-pub mod command_install;
-mod utils;
-pub mod command_clean;
--- archived/p2p/src/internal/mod.rs
+++
@@ -1,5 +0,0 @@
-
-pub mod dht_redis_store;
-
-#[cfg(test)]
-mod tests {}
