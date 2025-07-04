df78a347dbcf1adac9f77809a911e69f2b423639
Tianxing Ye <benjamin.747@outlook.com>
2025-07-04 11:41:39 +0800

    fix(UI):quick fix API err (#1191)

--- moon/apps/web/hooks/usePostMrList.ts
+++ moon/apps/web/hooks/usePostMrList.ts
@@ -1,10 +1,10 @@
 // hooks/usePostApiMrList.ts
 import { useMutation } from '@tanstack/react-query'
 import { legacyApiClient } from '@/utils/queryClient'
-import { PageParamsMRStatusParams, RequestParams, PostApiMrListData } from '@gitmono/types'
+import { PageParamsListPayload, RequestParams, PostApiMrListData } from '@gitmono/types'
 
 export function usePostMrList() {
-  return useMutation<PostApiMrListData, Error, { data: PageParamsMRStatusParams; params?: RequestParams }>({
+  return useMutation<PostApiMrListData, Error, { data: PageParamsListPayload; params?: RequestParams }>({
     mutationFn: ({ data, params }) =>
       legacyApiClient.v1.postApiMrList().request(data, params),
   })
