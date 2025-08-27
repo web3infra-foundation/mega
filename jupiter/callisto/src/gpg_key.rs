11126bcac259763608e01eeb4518c10517a25a4a
AidCheng <cn.aiden.cheng@gmail.com>
2025-08-27 10:34:35 +0100

    [jupiter]UPDATE:sync entity structure with migrations
    Signed-off-by: AidCheng <cn.aiden.cheng@gmail.com>

--- jupiter/callisto/src/gpg_key.rs
+++ jupiter/callisto/src/gpg_key.rs
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 #[sea_orm(table_name = "gpg_key")]
 pub struct Model {
     #[sea_orm(primary_key)]
-    pub id: i64,
+    pub id: i32,
     pub user_id: i64,
     #[sea_orm(column_type = "Text")]
     pub key_id: String,
