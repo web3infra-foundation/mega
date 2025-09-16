d71e7efbe8aea71b65cc5790d2cc09410cce44a0
Li WenLin <2839681263@qq.com>
2025-09-16 15:34:16 +0800

    feat(UI): update MergeBox (#1456)
    * chore(UI): add react-virtuoso package
    
    * feat(UI): update MergeBox component

---
+++ moon/apps/web/components/MrBox/hooks/useGetMergeBox.ts
@@ -0,0 +1,32 @@
+import { useAtom } from "jotai";
+import { atomFamily } from "jotai/utils";
+import { atomWithWebStorage } from "@/utils/atomWithWebStorage";
+import { useQuery } from "@tanstack/react-query";
+import { legacyApiClient } from "@/utils/queryClient";
+import { GetApiMrMergeBoxData } from "@gitmono/types";
+import { useEffect } from "react";
+
+const fetchMergeBox = legacyApiClient.v1.getApiMrMergeBox()
+const getMergeBoxAtom = atomFamily(() =>
+  atomWithWebStorage<GetApiMrMergeBoxData['data']>(`merge-box`, {})
+)
+
+export const useGetMergeBox = (link: string) => {
+  const [mergeBoxData, setMergeBoxData] = useAtom(getMergeBoxAtom(`merge-box`));
+  const { data, isLoading } = useQuery({
+    queryKey: fetchMergeBox.requestKey(link),
+    queryFn: async () => {
+      const response = await fetchMergeBox.request(link)
+
+      return response.data ?? {}
+    }
+  })
+
+  useEffect(() => {
+    if (data) {
+      setMergeBoxData(data)
+    }
+  }, [data, setMergeBoxData]);
+
+  return { mergeBoxData, isLoading };
+}
