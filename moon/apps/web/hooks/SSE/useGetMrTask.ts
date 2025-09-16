9f1cafc21b5f931ca7dedf2107a1d99dd523dfd6
derekLiu <116852328+liuyangjuncong20202570@users.noreply.github.com>
2025-09-16 11:24:37 +0800

    feat:update logic of sse (#1452)

--- moon/apps/web/hooks/SSE/useGetMrTask.ts
+++ moon/apps/web/hooks/SSE/useGetMrTask.ts
@@ -1,23 +1,36 @@
 import { useQuery } from '@tanstack/react-query'
 
+import { GetTasksByMrData } from '@gitmono/types/generated'
+
 import { fetchTask } from './ssmRequest'
 
-export interface TaskResult {
-  arguments: string
-  build_id: string
-  end_at: string
-  exit_code: number
-  mr: string
-  output_file: string
-  repo_name: string
-  start_at: string
-  target: string
+export interface Tasks {
+  build_list: BuildList[]
+  created_at: string
+  mr_id: number
+  task_id: string
+  task_name: string
+  template: string
 }
 
-export function useGetMrTask(mr: string) {
-  return useQuery<TaskResult[], Error>({
+export interface BuildList {
+  args: string[]
+  created_at: string
+  end_at: string
+  exit_code: number
+  id: string
+  output_file: string
+  repo: string
+  start_at: string
+  status: string
+  target: string
+  task_id: string
+}
+
+export function useGetMrTask(mr: number) {
+  return useQuery<GetTasksByMrData, Error>({
     queryKey: [mr],
-    queryFn: () => fetchTask(mr),
+    queryFn: () => fetchTask(mr)
     // refetchInterval: 15000,
     // refetchIntervalInBackground: true,
     // enabled: !!mr
