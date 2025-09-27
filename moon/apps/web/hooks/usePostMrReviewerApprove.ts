4eed3a3b735d995e7ce5b6ba08953d7fcfcaeb72
Yume <2839681263@qq.com>
2025-09-27 21:18:40 +0800

    feat(UI): update reviewer hooks

--- moon/apps/web/hooks/usePostMrReviewerApprove.ts
+++ moon/apps/web/hooks/usePostMrReviewerApprove.ts
@@ -1,29 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import { PostApiMrReviewerApproveData } from "@gitmono/types";
+import { useMutation } from "@tanstack/react-query";
+import { ChangeReviewerStatePayload, PostApiMrReviewerApproveData } from "@gitmono/types";
 import { legacyApiClient } from "@/utils/queryClient";
 
-export const usePostMrReviewerApprove = (
-  link: string,
-  approved: boolean
-): {
-  data: string,
-  isLoading: boolean
-} => {
-  const { data, isLoading } = useQuery<PostApiMrReviewerApproveData>({
-    // eslint-disable-next-line @tanstack/query/exhaustive-deps
-    queryKey: legacyApiClient.v1.postApiMrReviewerApprove().requestKey(link),
-    queryFn: async () => {
-      return await legacyApiClient.v1.postApiMrReviewerApprove().request(
-        link,
-        {
-          approved
-        }
-      )
-    }
+export const usePostMrReviewerApprove = () => {
+  return useMutation<PostApiMrReviewerApproveData, Error, {link: string, data: ChangeReviewerStatePayload}>({
+    mutationFn: ({link, data}) => legacyApiClient.v1.postApiMrReviewerApprove().request(link, data)
   })
-
-  return {
-    data: data?.data ?? "",
-    isLoading
-  }
 }
