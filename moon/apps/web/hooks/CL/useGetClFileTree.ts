2909a815581dff33d5d1ef29569625659c71ffd9
卷娄 <juanlou1217@163.com>
2025-10-15 22:40:03 +0800

    refactor: unify MR terminology to CL (Change List)

---
+++ moon/apps/web/hooks/CL/useGetClFileTree.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { legacyApiClient } from "@/utils/queryClient";
+
+export function useGetClFileTree(link: string) {
+  return useQuery({
+    queryKey: legacyApiClient.v1.getApiClMuiTree().requestKey(link),
+    queryFn: () => legacyApiClient.v1.getApiClMuiTree().request(link),
+    enabled: !!link,
+  })
+}
