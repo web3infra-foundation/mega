a850f1b020dd3204f6f4191fe2ee8f3fbab69145
Tianxing Ye <benjamin.747@outlook.com>
2025-11-05 16:30:21 +0800

    feat: Remove monobean (#1611)

--- monobean/src/utils.rs
+++
@@ -1,8 +0,0 @@
-#[macro_export]
-macro_rules! static_array {
-    ($type: ty, [$($elem:expr),* $(,)?]) => {{
-        const LEN: usize = 0 $(+ { let _ = $elem; 1 })*;
-        const ARRAY: [$type; LEN] = [$($elem),*];
-        ARRAY
-    }};
-}
