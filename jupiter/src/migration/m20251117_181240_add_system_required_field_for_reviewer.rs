4055a99f6b22fd3c55426fa1108a5d34528c0e36
Aiden <57825561+AidCheng@users.noreply.github.com>
2025-11-25 01:14:33 +0000

    [mono]ADD: System Required Reviewer (#1647)
    * [mono]FEAT: System Required Reviewer
    
    * [Jupiter]FIX: Set system_required default to false
    
    Signed-off-by: AidCheng <cn.aiden.cheng@gmail.com>
    
    * [jupiter]UPDATE: refactor storage service into one handler
    
    * UPDATE: sea-orm version
    
    Signed-off-by: AidCheng <cn.aiden.cheng@gmail.com>
    
    * [mono]REMOVE: modify system_required API
    
    Signed-off-by: AidCheng <cn.aiden.cheng@gmail.com>
    
    ---------
    
    Signed-off-by: AidCheng <cn.aiden.cheng@gmail.com>
    Signed-off-by: Aiden <57825561+AidCheng@users.noreply.github.com>
    Co-authored-by: Quanyi Ma <eli@patch.sh>

---
+++ jupiter/src/migration/m20251117_181240_add_system_required_field_for_reviewer.rs
@@ -0,0 +1,43 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(MegaMrReviewer::Table)
+                    .add_column(
+                        boolean(MegaMrReviewer::SystemRequired)
+                            .default(false)
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(MegaMrReviewer::Table)
+                    .drop_column(MegaMrReviewer::SystemRequired)
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+enum MegaMrReviewer {
+    #[sea_orm(iden = "mega_cl_reviewer")]
+    Table,
+    SystemRequired,
+}
