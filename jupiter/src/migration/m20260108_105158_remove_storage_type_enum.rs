9b15db0f988f42de18dc2a5b91713ce0a109dd76
tianyi <2739022972@qq.com>
2026-01-08 19:18:17 +0800

    fix:PostgreSQL-only migration

--- jupiter/src/migration/m20260108_105158_remove_storage_type_enum.rs
+++ jupiter/src/migration/m20260108_105158_remove_storage_type_enum.rs
@@ -1,5 +1,7 @@
-use sea_orm::{DatabaseBackend, Statement};
-use sea_orm_migration::prelude::*;
+use sea_orm_migration::{
+    prelude::*,
+    sea_orm::{DatabaseBackend, Statement},
+};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -7,32 +9,46 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .get_connection()
-            .execute(Statement::from_string(
-                DatabaseBackend::Postgres,
-                r#"DROP TYPE IF EXISTS storage_type_enum;"#.to_owned(),
-            ))
-            .await?;
+        let backend = manager.get_database_backend();
+
+        match backend {
+            DatabaseBackend::Postgres => {
+                manager
+                    .get_connection()
+                    .execute(Statement::from_string(
+                        DatabaseBackend::Postgres,
+                        "DROP TYPE IF EXISTS storage_type_enum;".to_owned(),
+                    ))
+                    .await?;
+            }
+            DatabaseBackend::Sqlite | DatabaseBackend::MySql => {}
+        }
 
         Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .get_connection()
-            .execute(Statement::from_string(
-                DatabaseBackend::Postgres,
-                r#"
-                    CREATE TYPE storage_type_enum AS ENUM (
-                        'database',
-                        'local_fs',
-                        'aws_s3'
-                    );
-                    "#
-                .to_owned(),
-            ))
-            .await?;
+        let backend = manager.get_database_backend();
+
+        match backend {
+            DatabaseBackend::Postgres => {
+                manager
+                    .get_connection()
+                    .execute(Statement::from_string(
+                        DatabaseBackend::Postgres,
+                        r#"
+                            CREATE TYPE storage_type_enum AS ENUM (
+                                'database',
+                                'local_fs',
+                                'aws_s3'
+                            );
+                            "#
+                        .to_owned(),
+                    ))
+                    .await?;
+            }
+            DatabaseBackend::Sqlite | DatabaseBackend::MySql => {}
+        }
 
         Ok(())
     }
