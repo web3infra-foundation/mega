90347b1868e6f510c127739ae620b53058bd5ffd
juanlou1217 <zhaokang.r2cn@isrc.iscas.ac.cn>
2026-02-01 17:37:39 +0800

    feat(theme): Complete theme color system migration for CL, Issue, Queue, OrionClient, and Settings modules

--- moon/apps/web/components/CodeView/Tags/NewMonoTagDialog.tsx
+++ moon/apps/web/components/CodeView/Tags/NewMonoTagDialog.tsx
@@ -18,9 +18,8 @@ import { Dialog } from '@gitmono/ui/Dialog'
 
 import { useCreateMonoTag } from '@/hooks/useCreateMonoTag'
 import { useGetCurrentUser } from '@/hooks/useGetCurrentUser'
-import { useGetTreeCommitInfo } from '@/hooks/useGetTreeCommitInfo'
-
 import { useGetLatestCommit } from '@/hooks/useGetLatestCommit'
+import { useGetTreeCommitInfo } from '@/hooks/useGetTreeCommitInfo'
 
 interface Props {
   open: boolean
