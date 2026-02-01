50b600428b49bdc04f544e3f2429be8c21eba55b
tianyi <2739022972@qq.com>
2026-02-01 15:25:03 +0800

    fix: rebase error

--- jupiter/src/migration/mod.rs
+++ jupiter/src/migration/mod.rs
@@ -148,11 +148,8 @@ impl MigratorTrait for Migrator {
             Box::new(m20260108_105158_remove_storage_type_enum::Migration),
             Box::new(m20260115_000000_create_targets_table::Migration),
             Box::new(m20260119_060233_add_mega_code_review::Migration),
-<<<<<<< HEAD
             Box::new(m20260127_081517_create_build_triggers::Migration),
-=======
             Box::new(m20260128_080549_add_mega_code_review_anchor_and_position::Migration),
->>>>>>> dba5dcef (refactor(code_review): add anchor and position to locat code thread)
         ]
     }
 }
