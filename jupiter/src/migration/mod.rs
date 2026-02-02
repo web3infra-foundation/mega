a83c55c315cf002517632cec52d01e28f6c29614
TOP <89700178+topshihun@users.noreply.github.com>
2026-02-02 18:59:36 +0800

    refactor: some modules (#4)
    * feat: add refactor orion module for migration
    
    Signed-off-by: Wangyan <topshihun@foxmail.com>
    
    * fix: add CL for build
    
    Signed-off-by: Wangyan <topshihun@foxmail.com>
    
    * refactor: some modules
    
    Signed-off-by: Wangyan <topshihun@foxmail.com>
    
    ---------
    
    Signed-off-by: Wangyan <topshihun@foxmail.com>
    Signed-off-by: Aid_C <57825561+AidCheng@users.noreply.github.com>
    Co-authored-by: Aid_C <57825561+AidCheng@users.noreply.github.com>
    Signed-off-by: AidCheng <cn.aiden.cheng@gmail.com>

--- jupiter/src/migration/mod.rs
+++ jupiter/src/migration/mod.rs
@@ -79,6 +79,7 @@ mod m20260108_105158_remove_storage_type_enum;
 mod m20260115_000000_create_targets_table;
 mod m20260119_060233_add_mega_code_review;
 mod m20260127_081517_create_build_triggers;
+mod m20260130_065535_refactor_orion_module;
 mod m20260128_080549_add_mega_code_review_anchor_and_position;
 
 /// Creates a primary key column definition with big integer type.
@@ -149,6 +150,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20260115_000000_create_targets_table::Migration),
             Box::new(m20260119_060233_add_mega_code_review::Migration),
             Box::new(m20260127_081517_create_build_triggers::Migration),
+            Box::new(m20260130_065535_refactor_orion_module::Migration),
             Box::new(m20260128_080549_add_mega_code_review_anchor_and_position::Migration),
         ]
     }
