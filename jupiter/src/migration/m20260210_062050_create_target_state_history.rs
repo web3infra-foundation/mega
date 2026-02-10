5534bdc11c79ac6d45691de3802ef39c29232a73
Wangyan <topshihun@foxmail.com>
2026-02-10 14:50:08 +0800

    feat: add table target_state_histories
    Signed-off-by: Wangyan <topshihun@foxmail.com>

---
+++ jupiter/src/migration/m20260210_062050_create_target_state_history.rs
@@ -0,0 +1,59 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // create target state histories table
+        manager
+            .create_table(
+                Table::create()
+                    .table(TargetStateHistories::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(TargetStateHistories::TargetId)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(TargetStateHistories::BuildId)
+                            .uuid()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(TargetStateHistories::TargetState)
+                            .text()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(TargetStateHistories::CreatedTime)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // drop target state histories table
+        manager
+            .drop_table(Table::drop().table(TargetStateHistories::Table).to_owned())
+            .await?;
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+enum TargetStateHistories {
+    Table,
+    TargetId,
+    BuildId,
+    TargetState,
+    CreatedTime,
+}
